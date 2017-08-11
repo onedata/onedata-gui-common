dbf05d11c6ca1829c67e25a37e4d059d6beff26a
Jakub Liput <jakub.liput@gmail.com>
2017-08-11 15:35:08 +0200

    VFS-3450 Updating copyright years

--- addon/components/one-icon.js
+++ addon/components/one-icon.js
@@ -10,7 +10,7 @@ const {
  * Typical usage: ``{{one-icon icon='home'}}``
  * @module components/one-icon
  * @author Jakub Liput
- * @copyright (C) 2016 ACK CYFRONET AGH
+ * @copyright (C) 2016-2017 ACK CYFRONET AGH
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
 export default Ember.Component.extend({
