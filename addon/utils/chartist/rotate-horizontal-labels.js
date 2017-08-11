5469ef0924777b44460e3e1dd2e2bff3f481722c
Jakub Liput <jakub.liput@gmail.com>
2017-08-11 15:57:44 +0200

    VFS-3450 Moving chartist plugins into ember-gui-common addon

---
+++ addon/utils/chartist/rotate-horizontal-labels.js
@@ -0,0 +1,26 @@
+/**
+ * Plugin for Chartist which rotates x axis labels by 45 degrees.
+ *
+ * @module utils/chartist/rotate-horizontal-labels
+ * @author Michal Borzecki
+ * @copyright (C) 2017 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ */
+
+export default function () {
+  return (chart) => {
+    chart.on('created', () => {
+      let labels = $(chart.container).find(
+        '.ct-labels .ct-label.ct-horizontal.ct-end').parent();
+      labels.each((index, element) => {
+        let label = $(element);
+        let width = parseFloat(label.attr('width'));
+        let rotateX = parseFloat(label.attr('x')) + width;
+        let rotateParams = `-45 ${rotateX} ${label.attr('y')}`;
+        let translateParams = `-${width / Math.SQRT2} -${width / Math.SQRT2}`;
+        label.attr('transform',
+          `rotate(${rotateParams}) translate(${translateParams})`);
+      });
+    });
+  };
+}
---
+++ addon/utils/chartist/tooltip.js
@@ -0,0 +1,172 @@
+/**
+ * Plugin for Chartist which adds tooltip. For bar and line charts tooltip 
+ * creates description based on chartist legend and values. For pie chart data for tooltip is 
+ * taken from data.series.tooltipElements. For example:
+ * ```
+ * tooltipElements: [{
+ *     name: 'prop1',
+ *     value: '100',
+ *   },
+ *   {
+ *     name: 'desc2',
+ *     value: '23%',
+ * }]
+ * ```
+ *
+ * Options:
+ * - chartType - type of the chart (bar, line, pie)
+ * - rangeInTitle - takes two x axis labels instead of one to tooltip title
+ * - renderAboveBarDescription - [bar chart only] if true, places tooltip 
+ * above a text instead of bar
+ * - topOffset - top offset of a tooltip
+ * - valueSuffix - [bar/line chart only] suffix for tooltip entries (e.g. for units)
+ * 
+ * @module utils/chartist/tooltip
+ * @author Michal Borzecki
+ * @copyright (C) 2017 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ */
+
+/* global Chartist */
+
+const TOOLTIP_HTML =
+  `
+  <div class="chart-tooltip">
+    <div class="chart-tooltip-title"></div>
+    <ul class="ct-legend">
+    </ul>
+    <div class="chart-tooltip-arrow"></div>
+  </div>
+`;
+
+export default function (options) {
+  let defaultOptions = {
+    chartType: 'bar',
+    rangeInTitle: false,
+    renderAboveBarDescription: false,
+    topOffset: -10,
+    valueSuffix: '',
+  };
+  options = Chartist.extend({}, defaultOptions, options);
+
+  return (chart) => {
+    let tooltipNode,
+      container = $(chart.container);
+
+    let prepareTooltip = function (tooltipData, data) {
+      // title
+      let title = tooltipNode.find('.chart-tooltip-title');
+      title.empty();
+      title.append(chart.data.labels[data.index]);
+      if (options.rangeInTitle) {
+        if (chart.data.labels[data.index + 1]) {
+          title.append(' - ' + chart.data.labels[data.index + 1]);
+        } else if (chart.data.lastLabel) {
+          title.append(' - ' + chart.data.lastLabel);
+        }
+      }
+
+      // data series and values
+      let ul = tooltipNode.find('.ct-legend');
+      ul.empty();
+      let suffix = options.valueSuffix ? ' ' + options.valueSuffix : '';
+      tooltipData.forEach(d => {
+        ul.append(`<li class="${d.className}">${d.name}: ${d.value + suffix}</li>`);
+      });
+    };
+
+    chart.on('created', () => {
+      tooltipNode = container.find('.chart-tooltip');
+      if (tooltipNode.length === 0) {
+        tooltipNode = $($.parseHTML(TOOLTIP_HTML));
+        container.append(tooltipNode);
+        tooltipNode.css('transform',
+          `translateY(-100%) translateY(${options.topOffset}px) translateX(-50%)`);
+      } else {
+        tooltipNode.removeClass('active');
+      }
+    });
+
+    chart.on('draw', function (data) {
+      let tooltipData = chart.data.series.map(s => ({
+        className: s.className,
+        name: s.name,
+        value: s.data[data.index],
+      }));
+
+      if (data.type === 'bar' && options.chartType === 'bar') {
+        let groupNode = $(data.group._node),
+          barNode = $(data.element._node);
+
+        barNode.mouseover(() => {
+          let lastGroupNode = groupNode.parent().children().last();
+          let lastGroupBar = $(lastGroupNode.children('line')[data.index]);
+
+          // top position
+          if (options.renderAboveBarDescription) {
+            let sumLabel = $(lastGroupNode.children('text')[data.index]);
+            tooltipNode.css('top', (sumLabel.offset().top - container.offset().top) +
+              'px');
+          } else {
+            tooltipNode.css('top', (lastGroupBar.offset().top - container.offset()
+              .top) + 'px');
+          }
+          // left position
+          let rect = lastGroupBar[0].getBoundingClientRect();
+          tooltipNode.css('left', (rect.left + rect.width / 2 - container.offset()
+            .left) + 'px');
+
+          prepareTooltip(tooltipData, data);
+
+          tooltipNode.addClass('active');
+        }).mouseout(() => {
+          tooltipNode.removeClass('active');
+        });
+      }
+      if (data.type === 'point' && options.chartType === 'line') {
+        let groupNode = $(data.group._node),
+          pointNode = $(data.element._node);
+
+        pointNode.mouseover(() => {
+          // top position
+          let rect = pointNode[0].getBoundingClientRect();
+          if (options.renderAboveBarDescription) {
+            let sumLabel = $(groupNode.children('text')[data.index]);
+            tooltipNode.css('top', (sumLabel.offset().top - container.offset().top) +
+              'px');
+          } else {
+            tooltipNode.css('top', (rect.top - container.offset()
+              .top) + 'px');
+          }
+          // left position
+          tooltipNode.css('left', (rect.left + rect.width / 2 - container.offset()
+            .left) + 'px');
+
+          prepareTooltip(tooltipData, data);
+
+          tooltipNode.addClass('active');
+        }).mouseout(() => {
+          tooltipNode.removeClass('active');
+        });
+      }
+      if (data.type === 'slice' && options.chartType === 'pie') {
+        data.series.tooltipElements.forEach(element => element.className =
+          'no-padding');
+        let tooltipData = data.series.tooltipElements;
+        let sliceNode = $(data.element._node);
+        sliceNode.mousemove((event) => {
+          tooltipNode.css('top', (event.pageY - container.offset().top - 10) +
+            'px');
+          tooltipNode.css('left', (event.pageX - container.offset()
+            .left) + 'px');
+
+          prepareTooltip(tooltipData, data);
+
+          tooltipNode.addClass('active');
+        }).mouseout(() => {
+          tooltipNode.removeClass('active');
+        });
+      }
+    });
+  };
+}
