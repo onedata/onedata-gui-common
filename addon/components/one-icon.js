50c6bc0ee5717b82e150919b36fdd0b08d7be3f0
Michal Borzecki <michal.borzecki@hotmail.com>
2017-08-16 14:00:31 +0200

    VFS-3478 providers-list component

--- addon/components/one-icon.js
+++ addon/components/one-icon.js
@@ -2,14 +2,15 @@ import Ember from 'ember';
 import layout from 'onedata-gui-common/templates/components/one-icon';
 
 const {
-  computed
+  computed,
+  observer
 } = Ember;
 
 /**
  * Inserts a icon from oneicons font.
  * Typical usage: ``{{one-icon icon='home'}}``
  * @module components/one-icon
- * @author Jakub Liput
+ * @author Jakub Liput, Michal Borzecki
  * @copyright (C) 2016-2017 ACK CYFRONET AGH
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
@@ -19,12 +20,38 @@ export default Ember.Component.extend({
   classNames: ['one-icon', 'oneicon'],
   classNameBindings: ['iconClass'],
 
-  // defaults
+  /**
+   * Icon name (from oneicons font, without `oneicon-` prefix)
+   * To inject.
+   * @type {string}
+   */
   icon: 'checkbox-x',
+
+  /**
+   * Icon color
+   * @type {string}
+   */
   color: '',
+
   addClass: '',
 
   iconClass: computed('icon', function () {
     return `oneicon-${this.get('icon')}`;
   }),
+
+  stylesObserver: observer('color', function () {
+    this._applyStyles();
+  }),
+
+  didInsertElement() {
+    this._super(...arguments);
+    this._applyStyles();
+  },
+
+  _applyStyles() {
+    let color = this.get('color');
+    this.$().css({
+      color,
+    });
+  },
 });
