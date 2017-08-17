1c1a5092ee54c29a4928bb18a362f23d3e29842f
Jakub Liput <jakub.liput@gmail.com>
2017-08-17 14:29:06 +0200

    VFS-3450 Preparing project for Bamboo tests

--- testem.js
+++ testem.js
@@ -1,12 +1,14 @@
 /* eslint-env node */
 module.exports = {
-  test_page: 'tests/index.html?hidepassed',
-  disable_watching: true,
-  launch_in_ci: [
-    'PhantomJS'
+  'framework': 'mocha',
+  'test_page': 'tests/index.html?hidepassed',
+  'report_file': 'tests/test-results.xml',
+  'disable_watching': true,
+  'launch_in_ci': [
+    'Firefox',
+    'Chrome',
+  ].concat(process.platform === 'darwin' ? ['Safari'] : []),
+  'launch_in_dev': [
+    'Chrome',
   ],
-  launch_in_dev: [
-    'PhantomJS',
-    'Chrome'
-  ]
 };
