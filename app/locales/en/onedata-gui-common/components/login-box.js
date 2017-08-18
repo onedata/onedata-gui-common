6675d5d779cacc800a6a922193ce980b9e0b6466
Jakub Liput <jakub.liput@gmail.com>
2017-08-18 14:22:16 +0200

    VFS-3450 Temporary login screen

--- app/locales/en/onedata-gui-common/components/login-box.js
+++ app/locales/en/onedata-gui-common/components/login-box.js
@@ -1,5 +1,7 @@
 export default {
-  controlPanel: 'control panel',
+  brandTitle: 'Onedata',
+  brandSubtitle: 'Unknown service',
   login: 'login',
   newAccountWillBeCreated: 'New account will be created automatically on first login',
+  sessionExpired: 'Your session has expired',
 };
