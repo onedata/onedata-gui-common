e03f7a47d946a0d72e2a1e09f1b3e4c9bde383a5
Michal Borzecki <michal.borzecki@hotmail.com>
2017-09-26 13:41:08 +0200

    VFS-3684 space cleaning bar component

--- addon/utils/bytes-to-string.js
+++ addon/utils/bytes-to-string.js
@@ -19,39 +19,49 @@ const KILO = 1000;
 function bytesToStringIEC(bytes) {
   let number = bytes;
   let unit = 'B';
+  let multiplicator = 1;
   if (bytes >= TIB) {
     unit = 'TiB';
     number = bytes / TIB;
+    multiplicator = TIB;
   } else if (bytes >= GIB) {
     unit = 'GiB';
     number = bytes / GIB;
+    multiplicator = GIB;
   } else if (bytes >= MIB) {
     unit = 'MiB';
     number = bytes / MIB;
+    multiplicator = MIB;
   } else if (bytes >= KIB) {
     unit = 'KiB';
     number = bytes / KIB;
+    multiplicator = KIB;
   }
-  return [number, unit];
+  return [number, multiplicator, unit];
 }
 
 function bytesToStringSI(bytes) {
   let number = bytes;
   let unit = 'B';
+  let multiplicator = 1;
   if (bytes >= TERA) {
     unit = 'TB';
     number = bytes / TERA;
+    multiplicator = TERA;
   } else if (bytes >= GIGA) {
     unit = 'GB';
     number = bytes / GIGA;
+    multiplicator = GIGA;
   } else if (bytes >= MEGA) {
     unit = 'MB';
     number = bytes / MEGA;
+    multiplicator = MEGA;
   } else if (bytes >= KILO) {
     unit = 'KB';
     number = bytes / KILO;
+    multiplicator = KILO;
   }
-  return [number, unit];
+  return [number, multiplicator, unit];
 }
 
 /**
@@ -62,12 +72,21 @@ function bytesToStringSI(bytes) {
  * @param {Object} [options]
  * @param {Boolean} [options.iecFormat=false] If true, use IEC format: KiB, MiB, GiB
  */
-export default function bytesToString(bytes, { iecFormat = false } = { iecFormat: false }) {
+export default function bytesToString(bytes, { iecFormat = false, separated = false } = { iecFormat: false, separated: false }) {
   if (!bytes && bytes !== 0) {
     return '';
   } else {
-    let [number, unit] =
+    let [number, multiplicator, unit] =
     (iecFormat ? bytesToStringIEC : bytesToStringSI)(bytes);
-    return `${Math.round(number * 10) / 10} ${unit}`;
+    number = Math.round(number * 10) / 10;
+    if (separated) {
+      return {
+        number,
+        multiplicator,
+        unit,
+      }
+    } else {
+      return `${number} ${unit}`;
+    }
   }
 }
