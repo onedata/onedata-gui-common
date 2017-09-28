7b9deb25e9ed204f1df2eecec32e0e2033020de9
Jakub Liput <jakub.liput@gmail.com>
2017-09-28 14:09:53 +0200

    VFS-3685 Preparing data and base components for files popularity and auto cleaning views

--- tests/dummy/app/components/demo-components/one-way-toggle.js
+++ tests/dummy/app/components/demo-components/one-way-toggle.js
@@ -6,16 +6,27 @@
  */
 
 import Ember from 'ember';
+import { Promise } from 'rsvp';
+import { run } from '@ember/runloop';
 
 export default Ember.Component.extend({
   // values for toggles
   twoStateToggle: false,
   threeStateToggle: 2,
   disabledToggle: true,
+  delayedToggle: false,
 
   actions: {
     toggle1FocusOut() {
       console.log('toggle1 focus out');
+    },
+    promisedToggle() {
+      return new Promise(resolve => {
+        run.later(() => {
+          this.toggleProperty('delayedToggle');
+          resolve();
+        }, 3000);
+      });
     }
   }
 });
