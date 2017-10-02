f7129b8311200dec04f7c1c0e36b0e0791c861d1
Jakub Liput <jakub.liput@gmail.com>
2017-10-02 12:09:41 +0200

    VFS-3685 Fixing plainable object problem with internal EmberObject

--- addon/utils/plain-copy.js
+++ addon/utils/plain-copy.js
@@ -1,18 +1,10 @@
 // Original implementation: http://stackoverflow.com/a/15002755
 
-import Ember from 'ember';
+import { isArray } from '@ember/array';
+import { get } from '@ember/object';
 
 // TODO: WARNING this function does not clone object deeply, 
 // it will just return always a plain object
-
-/**
- * Somehow hacky method to check if object is Ember Object
- * @param {object|Ember.Object} obj
- * @returns {boolean} true if obj is Ember.Object
- */
-function isEmberObject(obj) {
-  return obj.reopen !== undefined;
-}
 
 function emberObjPlainCopy(emberObj) {
   var props = Object.keys(emberObj);
@@ -24,20 +16,16 @@ function emberObjPlainCopy(emberObj) {
   }
   var copy = {};
   props.forEach(function (p) {
-    copy[p] = plainCopy(emberObj.get(p));
+    copy[p] = plainCopy(get(emberObj, p));
   }, emberObj);
   return copy;
 }
 
 export default function plainCopy(obj) {
-  if (Ember.isArray(obj)) {
+  if (isArray(obj)) {
     return obj.map(plainCopy);
-  } else if (typeof (obj) === 'object') {
-    if (isEmberObject(obj)) {
-      return emberObjPlainCopy(obj);
-    } else {
-      return obj;
-    }
+  } else if (obj && (typeof obj === 'object')) {
+    return emberObjPlainCopy(obj);
   } else {
     return obj;
   }
