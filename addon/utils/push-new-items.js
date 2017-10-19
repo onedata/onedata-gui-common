4f6450df8b6661766541f4000e70bd54e61ae7ac
Jakub Liput <jakub.liput@gmail.com>
2017-10-19 16:08:48 +0200

    VFS-3685 Auto cleaning reports now are updated instead of replaced

---
+++ addon/utils/push-new-items.js
@@ -0,0 +1,30 @@
+/**
+ * Adds new items to array without changing original array reference and
+ * reference to items that are deeply equal to these in new array.
+ *
+ * For detailed description see unit tests that covers all features.
+ *
+ * @module utils/push-new-items
+ * @author Jakub Liput
+ * @copyright (C) 2017 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ */
+
+import _ from 'lodash';
+
+export default function pushNewItems(orig, update, compare) {
+  const pushFun = orig.pushObject ? 'pushObject' : 'push';
+
+  _.forEach(update, uitem => {
+    const matchItemIndex = _.findIndex(orig, oitem => compare(oitem, uitem));
+    if (matchItemIndex >= 0) {
+      const matchItem = orig[matchItemIndex];
+      if (!_.isEqual(matchItem, uitem)) {
+        orig[matchItemIndex] = uitem;
+      }
+    } else {
+      orig[pushFun](uitem);
+    }
+  });
+  return orig;
+}
---
+++ tests/unit/utils/push-new-items-test.js
@@ -0,0 +1,62 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import { A } from '@ember/array';
+import pushNewItems from 'onedata-gui-common/utils/push-new-items';
+import sinon from 'sinon';
+
+import _ from 'lodash';
+
+function createArray(type, content) {
+  switch (type) {
+    case 'ember':
+      return A(content);
+    case 'plain':
+      return content;
+    default:
+      throw `Unsupported array type: ${type}`;
+  }
+}
+
+describe('Unit | Utility | push new items', function () {
+
+  ['plain', 'ember'].forEach(arrayType => {
+    it(`adds only new, updated items using compare fun. for ${arrayType} array`,
+      function () {
+        const a2 = { a: 2 };
+        const orig = createArray(arrayType, [
+          { a: 1 },
+          a2,
+          { a: 3 },
+          { a: 99 },
+        ]);
+        const update = createArray(arrayType, [
+          { a: 2 },
+          { a: 3, b: 'x' },
+          { a: 4, b: 'x' },
+        ]);
+
+        const result = pushNewItems(orig, update, (x, y) => x.a === y.a);
+
+        expect(result).to.equal(orig);
+        expect(result).to.have.lengthOf(5);
+
+        expect(_.find(result, { a: 1 })).to.deep.equal({ a: 1 });
+        expect(_.find(result, { a: 2 }), 'do not replace reference')
+          .to.equal(a2);
+        expect(_.filter(result, { a: 2 })).to.have.lengthOf(1);
+        expect(_.find(result, { a: 3 })).to.deep.equal({ a: 3, b: 'x' });
+        expect(_.filter(result, { a: 3 })).to.have.lengthOf(1);
+        expect(_.find(result, { a: 4 })).to.deep.equal({ a: 4, b: 'x' });
+        expect(_.find(result, { a: 99 })).to.deep.equal({ a: 99 });
+      });
+  });
+
+  it('uses pushObject on EmberArrays', function () {
+    const orig = A();
+    const pushObject = sinon.spy(orig, 'pushObject');
+
+    pushNewItems(orig, ['test'], (x, y) => x === y);
+
+    expect(pushObject).to.be.calledOnce;
+  });
+});
