c543d1f290929f58ee7641708944712577d6810b
Jakub Liput <jakub.liput@gmail.com>
2017-10-20 14:08:52 +0200

    VFS-3710 Fixing bytes-to-string tests

--- tests/unit/utils/bytes-to-string-test.js
+++ tests/unit/utils/bytes-to-string-test.js
@@ -4,27 +4,27 @@ import bytesToString from 'onedata-gui-common/utils/bytes-to-string';
 
 describe('Unit | Utility | bytes to string', function () {
   it('can convert byte size to GiB string', function () {
-    let result = bytesToString(1073741824, { iecFormat: true });
+    let result = bytesToString(Math.pow(1024, 3), { iecFormat: true });
     expect(result).to.be.equal('1 GiB');
   });
 
   it('can convert byte size to MiB string', function () {
-    let result = bytesToString(1048576, { iecFormat: true });
+    let result = bytesToString(Math.pow(1024, 2), { iecFormat: true });
     expect(result).to.be.equal('1 MiB');
   });
 
   it('can convert byte size to GB string', function () {
-    let result = bytesToString(1000000000);
-    expect(result).to.be.equal('1 GB', { iecFormat: false });
+    let result = bytesToString(Math.pow(1000, 3), { iecFormat: false });
+    expect(result).to.be.equal('1 GB');
   });
 
   it('can convert byte size to MB string', function () {
-    let result = bytesToString(1000000);
-    expect(result).to.be.equal('1 MB', { iecFormat: false });
+    let result = bytesToString(Math.pow(1000, 2), { iecFormat: false });
+    expect(result).to.be.equal('1 MB');
   });
 
   it('rounds size in string to 1 digit after comma by default (w/o IEC)', function () {
-    let result = bytesToString(1251111);
-    expect(result).to.be.equal('1.3 MB', { iecFormat: false });
+    let result = bytesToString(1251111, { iecFormat: false });
+    expect(result).to.be.equal('1.3 MB');
   });
 });
