e07d19ad28097ceb71b115b9bafd95886e5ad80e
Jakub Liput <jakub.liput@gmail.com>
2017-12-18 12:54:07 +0100

    VFS-3883 Ported split one-atlas/position component

--- addon/components/one-atlas/point.js
+++ addon/components/one-atlas/point.js
@@ -1,5 +1,5 @@
 /**
- * An one-atlas point component. Represents a place in a map, which 
+ * A one-atlas point component. Represents a place in a map, which 
  * should be specified with latitude and longitude properties.
  * Example of use can be found in one-atlas component.
  * 
@@ -9,32 +9,14 @@
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
 
-import Ember from 'ember';
+import Component from '@ember/component';
+import { observer } from '@ember/object';
 
 import layout from 'onedata-gui-common/templates/components/one-atlas/point';
 
-const {
-  computed,
-  observer,
-} = Ember;
-
-export default Ember.Component.extend({
+export default Component.extend({
   layout,
   classNames: ['one-atlas-point'],
-
-  /**
-   * Latitude (values -90 <= x <= 90, South < North)
-   * To inject.
-   * @type {number}
-   */
-  latitude: 0,
-
-  /**
-   * Longitude (values -180 <= x <= 180, West < East)
-   * To inject.
-   * @type {number}
-   */
-  longitude: 0,
 
   /**
    * Atlas parent component width.
@@ -52,61 +34,21 @@ export default Ember.Component.extend({
 
   /**
    * Point x position on atlas (in px)
-   * @type {computed.number}
+   * @virtual
+   * @type {number}
    */
-  _positionX: computed('atlasWidth', 'longitude', function () {
-    let {
-      atlasWidth,
-      longitude,
-    } = this.getProperties('atlasWidth', 'longitude');
-
-    if (longitude < -180 || longitude > 180) {
-      longitude = 0;
-    }
-    return ((longitude + 180) / 360) * atlasWidth;
-  }),
+  positionX: undefined,
 
   /**
    * Point y position on atlas (in px)
-   * @type {computed.number}
+   * @virtual
+   * @type {number}
    */
-  _positionY: computed('atlasHeight', 'latitude', function () {
-    let {
-      atlasHeight,
-      latitude,
-    } = this.getProperties('atlasHeight', 'latitude');
+  positionY: undefined,
 
-    if (latitude < -90 || latitude > 90) {
-      latitude = 0;
-    }
-    // Calculations based on https://en.wikipedia.org/wiki/Mercator_projection
-    // article
-    let ltr = latitude * (Math.PI / 180);
-    let y = 1.25 * Math.log(Math.tan(Math.PI / 4 + 0.4 * ltr));
-    return (atlasHeight / 2) * (1 - y * (1 / 2.303412543));
-  }),
-
-  _coordinatesValidatorObserver: observer('longitude', 'latitude', function () {
-    let {
-      longitude,
-      latitude,
-    } = this.getProperties('longitude', 'latitude');
-    if (longitude < -180 || longitude > 180) {
-      console.warn(`one-atlas/point: longitude out of range: ${longitude}`);
-    }
-    if (latitude < -90 || latitude > 90) {
-      console.warn(`one-atlas/point: latitude out of range: ${latitude}`);
-    }
-  }),
-
-  _positionObserver: observer('_positionX', '_positionY', function () {
+  _positionObserver: observer('positionX', 'positionY', function () {
     this._applyPosition();
   }),
-
-  init() {
-    this._super(...arguments);
-    this._coordinatesValidatorObserver();
-  },
 
   didInsertElement() {
     this._super(...arguments);
@@ -117,14 +59,14 @@ export default Ember.Component.extend({
    * Applies calculated position to CSS properties
    */
   _applyPosition() {
-    let {
-      _positionX,
-      _positionY,
-    } = this.getProperties('_positionX', '_positionY');
+    const {
+      positionX,
+      positionY,
+    } = this.getProperties('positionX', 'positionY');
 
     this.$().css({
-      top: _positionY,
-      left: _positionX,
+      top: positionY,
+      left: positionX,
     });
   }
 });
