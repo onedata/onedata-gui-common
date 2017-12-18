261b4390434be4365ad2ed0053179cbb484f2404
Jakub Liput <jakub.liput@gmail.com>
2017-12-18 13:18:04 +0100

    VFS-3883 Porting changes in one-pie-chart

--- addon/components/one-pie-chart.js
+++ addon/components/one-pie-chart.js
@@ -43,6 +43,7 @@ const SERIES_HOVER_TRANSITION_TIME = 0.3;
 export default Ember.Component.extend({
   layout,
   classNames: ['one-pie-chart'],
+  classNameBindings: ['_valuesSum::zero-chart'],
 
   /**
    * Data for a chart.
@@ -146,7 +147,7 @@ export default Ember.Component.extend({
    * Chartist chart series
    * @type {computed.Array.Object}
    */
-  _chartDataSeries: computed('_sortedData.@each.value', function () {
+  _chartDataSeries: computed('_sortedData.@each.value', '_valuesSum', function () {
     return this.generateChartDataSeries();
   }),
 
@@ -154,7 +155,7 @@ export default Ember.Component.extend({
    * Chartist chart pie labels
    * @type {computed.Array.string}
    */
-  _chartPieLabels: computed('_sortedData.@each.label', function () {
+  _chartPieLabels: computed('_sortedData.@each.label', '_valuesSum', function () {
     return this.generateChartPieLabels();
   }),
 
@@ -176,7 +177,7 @@ export default Ember.Component.extend({
         this.set('_stylesRecomputeTimeoutId', setTimeout(
           () => {
             if (!this.isDestroyed && !this.isDestroying) {
-              this.set('_chartCss', this.generateChartStyles())
+              this.set('_chartCss', this.generateChartStyles());
             }
           },
           hoverTransitionTime * 1000
@@ -253,6 +254,22 @@ export default Ember.Component.extend({
   },
 
   /**
+   * Options for centered text chartist plugin.
+   * @type {Object}
+   */
+  centeredTextOptions: {
+    text: '',
+  },
+
+  /**
+   * Options for legend colors chartist plugin.
+   * @type {Object}
+   */
+  legendColorsOptions: {
+    colors: [],
+  },
+
+  /**
    * Creates chartist options object.
    * @returns {Object} Chartist options.
    */
@@ -261,16 +278,24 @@ export default Ember.Component.extend({
       _mobileMode,
       _valuesSum,
       _sortedData,
-    } = this.getProperties('_mobileMode', '_valuesSum', '_sortedData');
+      centeredTextOptions,
+      legendColorsOptions,
+    } = this.getProperties(
+      '_mobileMode',
+      '_valuesSum',
+      '_sortedData',
+      'centeredTextOptions',
+      'legendColorsOptions'
+    );
+    centeredTextOptions.text = this.formatValue(_valuesSum);
+    legendColorsOptions.colors = _.map(_sortedData, 'color');
     let optionsBase = {
       donut: true,
       donutWidth: '45%',
       showLabel: false,
       chartPadding: 20,
       plugins: [
-        centeredText({
-          text: this.formatValue(_valuesSum),
-        }),
+        centeredText(centeredTextOptions),
         tooltip({
           chartType: 'pie',
         }),
@@ -283,9 +308,7 @@ export default Ember.Component.extend({
           className: 'not-clickable',
           clickable: false,
         }),
-        legendColors({
-          colors: _.map(_sortedData, 'color'),
-        }),
+        legendColors(legendColorsOptions),
       ]
     };
     if (_mobileMode) {
@@ -325,10 +348,17 @@ export default Ember.Component.extend({
    * @returns {Array.Object} Chartist data series.
    */
   generateChartDataSeries() {
-    let _sortedData = this.get('_sortedData');
-    return _sortedData.map((series) => {
+    const {
+      _sortedData,
+      _valuesSum,
+    } = this.getProperties('_sortedData', '_valuesSum');
+    // If each series == 0 (so _valuesSum == 0), then each series should be drawed 
+    // using the same value > 0 (here is 1) to give them the same space in chart
+    const valuesToDraw = _valuesSum ? _sortedData.mapBy('value') : _sortedData.map(() =>
+      1);
+    return _sortedData.map((series, index) => {
       return {
-        data: series.get('value'),
+        data: valuesToDraw[index],
         className: 'slice-id-' + series.get('id'),
         tooltipElements: [{
           name: 'Value',
@@ -410,7 +440,12 @@ export default Ember.Component.extend({
    * @returns {number} A value 0 < x <= 1
    */
   getSeriesPercentSize(series) {
-    return series.get('value') / this.get('_valuesSum');
+    const {
+      _valuesSum,
+      _sortedData,
+    } = this.getProperties('_valuesSum', '_sortedData');
+    return _valuesSum ? series.get('value') / this.get('_valuesSum') :
+      1 / _sortedData.get('length');
   },
 
   /**
