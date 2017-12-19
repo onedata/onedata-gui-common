109bf6cbdce101acba4525ecee6364512d405af5
Michal Borzecki <michal.borzecki@hotmail.com>
2017-12-19 12:55:18 +0100

    VFS-3619 ported oz-gui-default login header

--- addon/components/login-box.js
+++ addon/components/login-box.js
@@ -9,6 +9,7 @@
 
 import Ember from 'ember';
 import layout from 'onedata-gui-common/templates/components/login-box';
+import EmberObject from '@ember/object';
 
 const {
   inject: {
@@ -26,12 +27,35 @@ export default Ember.Component.extend({
   globalNotify: service(),
   session: service(),
 
+  /**
+   * Description for error (if occurred).
+   * @type {string|undefined}
+   */
+  errorMessage: undefined,
+
+  /**
+   * If true, data necessary to render login-box is still loading
+   * @type {boolean}
+   */
+  isLoading: false,
+
+  /**
+   * Data object passed to the login-box header component
+   * @type {EmberObject}
+   */
+  headerModel: undefined,
+
   isBusy: false,
 
   /**
    * True, if previous session has expired
    */
   sessionHasExpired: alias('session.data.hasExpired'),
+
+  init() {
+    this._super(...arguments);
+    this.set('headerModel', EmberObject.create({}));
+  },
 
   actions: {
     authenticationStarted() {
