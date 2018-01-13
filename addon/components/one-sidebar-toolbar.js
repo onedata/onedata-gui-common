8512e144c1e7ca2dfa3e39f45c721e626b3ae6c3
Michal Borzecki <michal.borzecki@hotmail.com>
2018-01-13 15:01:34 +0100

    VFS-3955 model mock fixes and code clean

--- addon/components/one-sidebar-toolbar.js
+++ addon/components/one-sidebar-toolbar.js
@@ -1,3 +1,12 @@
+/**
+ * Container component for sidebar buttons.
+ *
+ * @module services/one-sidebar-toolbar
+ * @author Jakub Liput
+ * @copyright (C) 2017-2018 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ */
+
 import Ember from 'ember';
 import layout from 'onedata-gui-common/templates/components/one-sidebar-toolbar';
 
