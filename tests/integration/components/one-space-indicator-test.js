3280ff9442a2e5e31bb877ce72e27fa3bc9c8e41
Michal Borzecki <michal.borzecki@hotmail.com>
2018-01-31 18:15:59 +0100

    VFS-3985 one-space-indicator used in spaces details

--- tests/integration/components/one-space-indicator-test.js
+++ tests/integration/components/one-space-indicator-test.js
@@ -9,23 +9,24 @@ describe('Integration | Component | one space indicator', function () {
   });
 
   it('renders space usage', function () {
-    this.render(hbs`{{one-space-indicator totalSize=2048 occupiedSize=1024}}`);
+    this.render(hbs `{{one-space-indicator totalSize=2048 occupiedSize=1024}}`);
 
     expect(this.$('.occupied-space-bar')).to.exist;
     expect(this.$('.total-space').text().trim()).to.equal('2 KiB');
     expect(this.$('.occupied-space').text().trim()).to.equal('1 KiB');
   });
 
-  it('ignores occupied space greater than total space', function () {
-    this.render(hbs`{{one-space-indicator totalSize=2048 occupiedSize=4096}}`);
+  it('recognizes occupied space greater than total space', function () {
+    this.render(hbs `{{one-space-indicator totalSize=2048 occupiedSize=4096}}`);
 
+    expect(this.$('.one-space-indicator')).to.have.class('critical-usage');
     expect(this.$('.occupied-space-bar')).to.exist;
     expect(this.$('.total-space').text().trim()).to.equal('2 KiB');
-    expect(this.$('.occupied-space').text().trim()).to.equal('2 KiB');
+    expect(this.$('.occupied-space').text().trim()).to.equal('4 KiB');
   });
 
   it('ignores occupied space less than 0', function () {
-    this.render(hbs`{{one-space-indicator totalSize=2048 occupiedSize=-10}}`);
+    this.render(hbs `{{one-space-indicator totalSize=2048 occupiedSize=-10}}`);
 
     expect(this.$('.occupied-space-bar')).to.not.exist;
     expect(this.$('.total-space').text().trim()).to.equal('2 KiB');
@@ -33,7 +34,7 @@ describe('Integration | Component | one space indicator', function () {
   });
 
   it('ignores total space less than 0', function () {
-    this.render(hbs`{{one-space-indicator totalSize=-10 occupiedSize=1024}}`);
+    this.render(hbs `{{one-space-indicator totalSize=-10 occupiedSize=1024}}`);
 
     expect(this.$('.occupied-space-bar')).to.not.exist;
     expect(this.$('.total-space')).to.not.exist;
