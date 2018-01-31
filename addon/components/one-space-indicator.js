3ffd27deed8befa31667ff1dec6b825ae99cbbd5
Michal Borzecki <michal.borzecki@hotmail.com>
2018-01-31 13:07:11 +0100

    VFS-3985 finished one-space-indicator component

--- addon/components/one-space-indicator.js
+++ addon/components/one-space-indicator.js
@@ -1,5 +1,15 @@
+/**
+ * A bar chart that shows used space according to given totalSize and occupiedSize.
+ *
+ * @module components/one-space-indicator
+ * @author Michal Borzecki
+ * @copyright (C) 2018 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ */
+
 import Component from '@ember/component';
 import { computed } from '@ember/object';
+import { htmlSafe } from '@ember/string';
 import layout from '../templates/components/one-space-indicator';
 
 export default Component.extend({
@@ -31,12 +41,11 @@ export default Component.extend({
    */
   _isOccupiedSizeValid: computed('_isTotalSizeValid', 'occupiedSize', function () {
     const {
-      totalSize,
       occupiedSize,
       _isTotalSizeValid,
-     } = this.getProperties('totalSize', 'occupiedSize', '_isTotalSizeValid');
-    return _isTotalSizeValid && typeof occupiedSize === 'number' &&
-      occupiedSize >= 0 && occupiedSize <= totalSize;
+     } = this.getProperties('occupiedSize', '_isTotalSizeValid');
+    return _isTotalSizeValid && typeof occupiedSize === 'number'&&
+      occupiedSize >= 0;
   }),
 
   /**
@@ -49,7 +58,8 @@ export default Component.extend({
       occupiedSize,
     } = this.getProperties('_isOccupiedSizeValid', 'totalSize', 'occupiedSize');
     
-    return _isOccupiedSizeValid ? (occupiedSize / totalSize) * 100 : undefined;
+    return _isOccupiedSizeValid ?
+      (Math.min(occupiedSize, totalSize) / totalSize) * 100 : undefined;
   }),
 
   /**
@@ -57,6 +67,25 @@ export default Component.extend({
    */
   _occupiedSpaceBarStyle: computed('_occupiedPercents', function () {
     const _occupiedPercents = this.get('_occupiedPercents');
-    return _occupiedPercents === undefined ? '' : `width: ${_occupiedPercents}%`;
+    return htmlSafe(
+      _occupiedPercents === undefined ? '' : `width: ${_occupiedPercents}%`
+    );
+  }),
+
+  /**
+   * @type {Ember.ComputedProperty<string>}
+   */
+  _occupiedSpaceLabelStyle: computed('_occupiedPercents', function () {
+    const _occupiedPercents = this.get('_occupiedPercents');
+    const labelMargin = 15;
+    if (_occupiedPercents !== undefined) {
+      const translateX = 
+        _occupiedPercents > labelMargin ? -100 : 0;
+      return htmlSafe(
+        `left: ${_occupiedPercents}%; transform: translateX(${translateX}%);`
+      );
+    } else {
+      return htmlSafe('');
+    }
   }),
 });
---
+++ tests/integration/components/one-space-indicator-test.js
@@ -0,0 +1,42 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import { setupComponentTest } from 'ember-mocha';
+import hbs from 'htmlbars-inline-precompile';
+
+describe('Integration | Component | one space indicator', function () {
+  setupComponentTest('one-space-indicator', {
+    integration: true
+  });
+
+  it('renders space usage', function () {
+    this.render(hbs`{{one-space-indicator totalSize=2048 occupiedSize=1024}}`);
+
+    expect(this.$('.occupied-space-bar')).to.exist;
+    expect(this.$('.total-space').text().trim()).to.equal('2 KiB');
+    expect(this.$('.occupied-space').text().trim()).to.equal('1 KiB');
+  });
+
+  it('ignores occupied space greater than total space', function () {
+    this.render(hbs`{{one-space-indicator totalSize=2048 occupiedSize=4096}}`);
+
+    expect(this.$('.occupied-space-bar')).to.exist;
+    expect(this.$('.total-space').text().trim()).to.equal('2 KiB');
+    expect(this.$('.occupied-space').text().trim()).to.equal('2 KiB');
+  });
+
+  it('ignores occupied space less than 0', function () {
+    this.render(hbs`{{one-space-indicator totalSize=2048 occupiedSize=-10}}`);
+
+    expect(this.$('.occupied-space-bar')).to.not.exist;
+    expect(this.$('.total-space').text().trim()).to.equal('2 KiB');
+    expect(this.$('.occupied-space')).to.not.exist;
+  });
+
+  it('ignores total space less than 0', function () {
+    this.render(hbs`{{one-space-indicator totalSize=-10 occupiedSize=1024}}`);
+
+    expect(this.$('.occupied-space-bar')).to.not.exist;
+    expect(this.$('.total-space')).to.not.exist;
+    expect(this.$('.occupied-space')).to.not.exist;
+  });
+});
