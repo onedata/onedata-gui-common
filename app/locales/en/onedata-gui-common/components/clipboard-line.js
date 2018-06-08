4d0bef22c3bb74f89373e3a20460a256569fd578
Jakub Liput <jakub.liput@gmail.com>
2018-06-08 09:39:23 +0200

    VFS-4435 New cluster screens; API update; removing host

---
+++ app/locales/en/onedata-gui-common/components/clipboard-line.js
@@ -0,0 +1,6 @@
+export default {
+  copySuccess: '{{textType}} copied to clipboard.',
+  copyFailure: 'Failed to copy {{textType}} to clipboard - please do it manually.',
+  copyBtn: 'Copy',
+  defaultTextType: 'text',
+};
