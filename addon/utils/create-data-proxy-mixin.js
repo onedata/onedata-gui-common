ba63be91d81b6677fc4618a132e74416a5ed6969
Jakub Liput <jakub.liput@gmail.com>
2018-09-04 17:29:38 +0200

    VFS-4700 Experimental getDataProxy method for create-data-proxy

--- addon/utils/create-data-proxy-mixin.js
+++ addon/utils/create-data-proxy-mixin.js
@@ -5,13 +5,25 @@
  * You should provide `fetch` method or implement `fetchName` method in object
  * that will return Promise that resolves data.
  * 
- * Properties and methods added to object:
+ * Properties and methods added to object - data will be replaced with provided
+ * `name` (eg. `updateDataProxy` -> `updateSomeNameProxy`):
  * 
  * ```
- * updateDataProxy(): Promise<T>
- * fetchData(): Promise<T> // implement or provide `fetch`
+ * // creates proxy using `fetchData` or replaces content of proxy
+ * updateDataProxy(replace: boolean, ...fetchArgs): Promise<T>
+ * 
+ * // implement or provide `fetch`, do actual data fetching
+ * fetchData(...fetchArgs): Promise<T>
+ * 
+ * // PromiseObject with data, can be undefined if `updateDataProxy` or
+ * // `getDataProxy` was not invoked yet
  * dataProxy: PromiseObject<T>
+ * 
+ * // object with data, can be undefined if data not yet fetched or rejected
  * data: T
+ * 
+ * // returns dataProxy or creates it when not created yet
+ * getDataProxy(reload: boolean, ...fetchArgs): PromiseObject<T>
  * ```
  * 
  * @module utils/create-data-proxy-mixin
@@ -29,10 +41,14 @@ import safeExec from 'onedata-gui-common/utils/safe-method-execution';
 
 export default function createDataProxyMixin(name, fetch = notImplementedReject) {
   return Mixin.create({
-    [`update${classify(name)}Proxy`](reload = false) {
-      const promise = this[`fetch${classify(name)}`]();
+    /**
+     * If `replace` is true, do not create new ProxyObject, but
+     * replace content, so `isPending` of the ProxyObject will not be changed
+     */
+    [`update${classify(name)}Proxy`](replace = false, ...fetchArgs) {
+      const promise = this[`fetch${classify(name)}`](...fetchArgs);
       const proxyProperty = `${camelize(name)}Proxy`;
-      if (reload) {
+      if (replace && this.get(`${proxyProperty}.content`)) {
         return promise
           .catch(error => {
             safeExec(this, 'set', `${proxyProperty}.content`, undefined);
@@ -59,5 +75,14 @@ export default function createDataProxyMixin(name, fetch = notImplementedReject)
     [`${camelize(name)}Proxy`]: null,
 
     [name]: reads(`${camelize(name)}Proxy.content`),
+
+    [`get${classify(name)}Proxy`](reload = false, ...args) {
+      const dataProxy = this.get(`${camelize(name)}Proxy`);
+      if (!reload && dataProxy) {
+        return dataProxy;
+      } else {
+        return this[`update${classify(name)}Proxy`](true, ...args);
+      }
+    },
   });
 }
--- tests/unit/utils/create-data-proxy-mixin-test.js
+++ tests/unit/utils/create-data-proxy-mixin-test.js
@@ -22,4 +22,58 @@ describe('Unit | Utility | create data proxy mixin', function () {
       });
     }
   );
+
+  it('has getDataProxy method which will generate new proxy if not found', function () {
+    const data = 'hello';
+    const fetch = sinon.stub().resolves(data);
+
+    const mixin = createDataProxyMixin('world', fetch);
+    const obj = EmberObject.extend(mixin, {}).create();
+
+    obj.getWorldProxy();
+    return wait()
+      .then(() => {
+        expect(fetch).to.be.calledOnce;
+        expect(get(obj, 'worldProxy.isFulfilled')).to.be.true;
+        expect(get(obj, 'world')).to.equal(data);
+        obj.getWorldProxy();
+      })
+      .then(() => {
+        expect(fetch).to.be.calledOnce;
+      });
+  });
+
+  it('has getDataProxy method which can force reload proxy', function () {
+    const fetch = sinon.stub()
+      .onFirstCall().resolves(1)
+      .onSecondCall().resolves(2);
+
+    const mixin = createDataProxyMixin('world', fetch);
+    const obj = EmberObject.extend(mixin, {}).create();
+
+    obj.getWorldProxy(true);
+    return wait()
+      .then(() => {
+        expect(fetch).to.be.calledOnce;
+        expect(get(obj, 'world')).to.equal(1);
+        obj.getWorldProxy(true);
+      })
+      .then(() => {
+        expect(fetch).to.be.calledTwice;
+        expect(get(obj, 'world')).to.equal(2);
+      });
+  });
+
+  it('passes arguments in updateDataProxy to fetch function', function () {
+    const fetch = sinon.stub().resolves('hello');
+
+    const mixin = createDataProxyMixin('world', fetch);
+    const obj = EmberObject.extend(mixin, {}).create();
+    obj.updateWorldProxy(false, 1, 2);
+
+    return wait().then(() => {
+      expect(fetch).to.be.calledOnce;
+      expect(fetch).to.be.calledWith(1, 2);
+    });
+  });
 });
