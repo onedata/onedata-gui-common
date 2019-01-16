bf96b2dcd1e57d426f066dee68e8bdf2053dac17
Jakub Liput <jakub.liput@gmail.com>
2019-01-16 15:59:21 +0100

    VFS-4596 Using Dart Sass compiler

--- addon/utils/define-sass-breakpoints.js
+++ addon/utils/define-sass-breakpoints.js
@@ -3,13 +3,13 @@
  *
  * @module utils/define-sass-breakpoints
  * @author Jakub Liput
- * @copyright (C) 2018 ACK CYFRONET AGH
+ * @copyright (C) 2018-2019 ACK CYFRONET AGH
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
 
 /* eslint-env node */
 
-const sass = require('node-sass');
+const sass = require('sass');
 
 /**
  * @param {EmberApp} app 
