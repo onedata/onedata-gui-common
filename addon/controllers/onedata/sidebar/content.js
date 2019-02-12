6efb59e96eeb741f665868935126c00e252bcec0
Jakub Liput <jakub.liput@gmail.com>
2019-02-12 12:43:35 +0100

    VFS-4596 Support for optional redirection in redirect-mixined routes

---
+++ addon/controllers/onedata/sidebar/content.js
@@ -0,0 +1,14 @@
+/**
+ * Handle query params
+ *
+ * @module controllers/onedata/content
+ * @author Jakub Liput
+ * @copyright (C) 2019 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ */
+
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+  queryParams: ['back_forward'],
+});
