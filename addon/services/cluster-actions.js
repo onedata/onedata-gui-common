11f2ec209e4d6941aaa7e37dd52a50dc4d7f28fb
Michał Borzęcki <michal.borzecki@hotmail.com>
2019-03-27 20:37:05 +0100

    VFS-5340 added join cluster button

--- addon/services/cluster-actions.js
+++ addon/services/cluster-actions.js
@@ -9,6 +9,7 @@
 
 import Service, { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
+import { collect } from '@ember/object/computed';
 import I18n from 'onedata-gui-common/mixins/components/i18n';
 
 export default Service.extend(I18n, {
@@ -20,9 +21,7 @@ export default Service.extend(I18n, {
   /**
    * @type {Ember.Computed<Array<SidebarButtonDefinition>>}
    */
-  buttons: computed('btnAdd', function buttons() {
-    return [this.get('btnAdd')];
-  }),
+  buttons: collect('btnAdd', 'btnJoin'),
 
   addAction: computed(function addAction() {
     const router = this.get('router');
@@ -36,6 +35,27 @@ export default Service.extend(I18n, {
       tip: this.t('btnAdd.hint'),
       class: 'add-cluster-btn',
       action: this.get('addAction'),
+    };
+  }),
+
+  /**
+   * @type {Ember.ComputedProperty<Function>}
+   */
+  joinAction: computed(function joinAction() {
+    const router = this.get('router');
+    return () => router.transitionTo('onedata.sidebar.content', 'clusters', 'join');
+  }),
+
+  /**
+   * @type {Ember.Computed<SidebarButtonDefinition>}
+   */
+  btnJoin: computed(function getBtnCreate() {
+    return {
+      icon: 'join-plug',
+      title: this.t('btnJoin.title'),
+      tip: this.t('btnJoin.hint'),
+      class: 'join-cluster-btn',
+      action: this.get('joinAction'),
     };
   }),
 });
