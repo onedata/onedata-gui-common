e1c1931bbe981faa142f14f8742ca215473575ee
Jakub Liput <jakub.liput@gmail.com>
2019-04-11 14:35:18 +0200

    VFS-4640 Added common translations

--- app/locales/en/onedata-gui-common/components/login-box.js
+++ app/locales/en/onedata-gui-common/components/login-box.js
@@ -1,8 +1,9 @@
 import header from './login-box/header';
+import loginFormContainer from './login-box/login-form-container';
 
 export default {
   header,
+  loginFormContainer,
   login: 'Login',
   loginExternal: 'Log in to',
-  sessionExpired: 'Your session has expired',
 };
---
+++ app/locales/en/onedata-gui-common/components/login-box/login-form-container.js
@@ -0,0 +1,3 @@
+export default {
+  sessionExpired: 'Your session has expired',
+}
