b4117a6205e29c0b243631bd9cb6532817ccfbea
Jakub Liput <jakub.liput@gmail.com>
2019-06-17 09:34:43 +0200

    VFS-5461 Fixed one-tab-bar demo

--- tests/dummy/app/components/demo-components/one-tab-bar.js
+++ tests/dummy/app/components/demo-components/one-tab-bar.js
@@ -6,30 +6,35 @@
  */
 
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 import _ from 'lodash';
 
 export default Component.extend({
-  items: Object.freeze(_.range(1, 20).map(i => ({
-    id: String(i),
-    name: i + '. Oneprovider with long name',
-    icon: 'provider',
-    class: 'provider-online',
-  }))),
-
-  fewItems: Object.freeze([{
-      id: '1',
-      name: 'Kraków',
+  items: computed(function items() {
+    return _.range(1, 20).map(i => ({
+      id: String(i),
+      name: i + '. Oneprovider with long name',
       icon: 'provider',
-    },
-    {
-      id: '2',
-      name: 'Paris',
-      icon: 'space',
-    },
-    {
-      id: '3',
-      name: 'Lisbon',
-      icon: 'group',
-    }
-  ]),
+      class: 'provider-online',
+    }));
+  }),
+
+  fewItems: computed(function fewItems() {
+    return [{
+        id: '1',
+        name: 'Kraków',
+        icon: 'provider',
+      },
+      {
+        id: '2',
+        name: 'Paris',
+        icon: 'space',
+      },
+      {
+        id: '3',
+        name: 'Lisbon',
+        icon: 'group',
+      }
+    ];
+  }),
 });
