af178d55f96f1e12f937fd00a0625dd5cea004f7
Michał Borzęcki <michal.borzecki@hotmail.com>
2019-08-28 17:48:47 +0200

    VFS-1891 added perfect-scrollbar to modal body

---
+++ addon/components/bs-modal/body.js
@@ -0,0 +1,6 @@
+import Body from 'ember-bootstrap/components/bs-modal/body';
+import layout from 'onedata-gui-common/templates/components/bs-modal/body';
+
+export default Body.extend({
+  layout,
+});
