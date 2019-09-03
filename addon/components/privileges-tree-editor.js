26d1cf32b75e6a9bff79fece480f57910d7c4ddb
Michał Borzęcki <michal.borzecki@hotmail.com>
2019-09-03 14:17:46 +0200

    VFS-5591 pr fixes

--- addon/components/privileges-tree-editor.js
+++ addon/components/privileges-tree-editor.js
@@ -85,7 +85,7 @@ export default Component.extend({
     'privilegesGroups',
     'privilegeGroupsTranslationsPath',
     'privilegesTranslationsPath',
-    function () {
+    function initialTreeState() {
       const {
         initialPrivileges,
         privilegesGroups,
@@ -132,23 +132,30 @@ export default Component.extend({
    * Tree paths, which are disabled for edition. Used to block tree edition.
    * @type {Ember.ComputedProperty<Ember.Array<string>>}
    */
-  treeDisabledPaths: computed('editionEnabled', 'privilegesGroups', function () {
-    const {
-      editionEnabled,
-      privilegesGroups,
-    } = this.getProperties('editionEnabled', 'privilegesGroups');
-    return editionEnabled ? A() : A(privilegesGroups.map(g => g.groupName));
-  }),
-
-  overridePrivilegesObserver: observer('overridePrivileges', function () {
-    const {
-      overridePrivileges,
-      initialPrivileges,
-    } = this.getProperties('overridePrivileges', 'initialPrivileges');
-    if (!initialPrivileges && overridePrivileges) {
-      this.set('initialPrivileges', overridePrivileges);
+  treeDisabledPaths: computed(
+    'editionEnabled',
+    'privilegesGroups',
+    function treeDisabledPaths() {
+      const {
+        editionEnabled,
+        privilegesGroups,
+      } = this.getProperties('editionEnabled', 'privilegesGroups');
+      return editionEnabled ? A() : A(privilegesGroups.map(g => g.groupName));
     }
-  }),
+  ),
+
+  overridePrivilegesObserver: observer(
+    'overridePrivileges',
+    function overridePrivilegesObserver() {
+      const {
+        overridePrivileges,
+        initialPrivileges,
+      } = this.getProperties('overridePrivileges', 'initialPrivileges');
+      if (!initialPrivileges && overridePrivileges) {
+        this.set('initialPrivileges', overridePrivileges);
+      }
+    }
+  ),
 
   init() {
     this._super(...arguments);
