76091a14b57f1ba019200b64e7e21d10e38850e0
Michał Borzęcki <michal.borzecki@hotmail.com>
2020-03-02 15:53:43 +0100

    VFS-6165 added clipboard field

---
+++ tests/integration/utils/form-component/clipboard-field-test.js
@@ -0,0 +1,26 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import ClipboardField from 'onedata-gui-common/utils/form-component/clipboard-field';
+import { get } from '@ember/object';
+import { setupComponentTest } from 'ember-mocha';
+
+describe('Integration | Utility | form component/clipboard field', function () {
+  setupComponentTest('test-component', {
+    integration: true,
+  });
+
+  it('defines fieldComponentName as "form-component/clipboard-field"', function () {
+    const field = ClipboardField.create();
+    expect(get(field, 'fieldComponentName')).to.equal('form-component/clipboard-field');
+  });
+
+  it('defines default type as "input"', function () {
+    const field = ClipboardField.create();
+    expect(get(field, 'type')).to.equal('input');
+  });
+
+  it('defines default textareaRows as 5', function () {
+    const field = ClipboardField.create();
+    expect(get(field, 'textareaRows')).to.equal(5);
+  });
+});
