abad11414a2839e42da687718fb4ab8b43795ae1
Jakub Liput <jakub.liput@gmail.com>
2020-03-31 10:34:40 +0200

    VFS-6145 Added tooltipTriggerEvents customization in remove-icon

--- addon/components/remove-icon.js
+++ addon/components/remove-icon.js
@@ -34,6 +34,12 @@ export default Component.extend({
    */
   onClick: notImplementedIgnore,
 
+  /**
+   * @virtual optional
+   * @type {boolean}
+   */
+  tooltipTriggerEvents: undefined,
+
   click() {
     const {
       isDisabled,
