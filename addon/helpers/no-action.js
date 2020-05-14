52be1bfb237962383a2a182d34c6db26c751eeaa
Jakub Liput <jakub.liput@gmail.com>
2020-05-14 11:43:11 +0200

    VFS-6323 no-action helper

---
+++ addon/helpers/no-action.js
@@ -0,0 +1,17 @@
+/**
+ * No-op function for templates
+ * 
+ * @module helpers/no-action
+ * @author Jakub Liput
+ * @copyright (C) 2020 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ */
+
+import { helper } from '@ember/component/helper';
+import notImplementedIgnore from 'onedata-gui-common/utils/not-implemented-ignore';
+
+export function noAction() {
+  return notImplementedIgnore;
+}
+
+export default helper(noAction);
---
+++ app/helpers/no-action.js
@@ -0,0 +1,1 @@
+export { default, noAction } from 'onedata-gui-common/helpers/no-action';
