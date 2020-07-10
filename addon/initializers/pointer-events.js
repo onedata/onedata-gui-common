1695d55802e8bff82a25c566cb8935d0895d1bd6
Jakub Liput <jakub.liput@gmail.com>
2020-07-10 13:07:03 +0200

    WIP (onezone)

---
+++ addon/initializers/pointer-events.js
@@ -0,0 +1,17 @@
+/**
+ * Injects i18n service into some factories. See initialize function for details.
+ *
+ * @module initializers/i18n
+ * @author Jakub Liput
+ * @copyright (C) 2016-2020 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ */
+
+export default {
+  name: 'pointer-events',
+
+  initialize: function (application) {
+    application.inject('component', 'pointerEvents', 'service:pointer-events');
+    application.inject('controller', 'pointerEvents', 'service:pointer-events');
+  },
+};
