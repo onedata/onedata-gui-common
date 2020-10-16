e5e8f06662abb459b25556d76b2b578ba668602c
Michał Borzęcki <michal.borzecki@hotmail.com>
2020-10-16 13:53:01 +0200

    VFS-6745 made dropdown field not render in place

--- addon/components/one-dropdown.js
+++ addon/components/one-dropdown.js
@@ -9,9 +9,29 @@
 
 import PowerSelect from 'ember-power-select/components/power-select';
 import safeExec from 'onedata-gui-common/utils/safe-method-execution';
+import { observer } from '@ember/object';
+import { inject as service } from '@ember/service';
+import notImplementedIgnore from 'onedata-gui-common/utils/not-implemented-ignore';
 
 export default PowerSelect.extend({
+  scrollState: service(),
+
+  scrollObserver: observer('scrollState.lastScrollEvent', function scrollObserver() {
+    this.handlePageScroll();
+  }),
+
+  init() {
+    this._super(...arguments);
+    this.get('scrollState.lastScrollEvent');
+  },
+
   updateState() {
     return safeExec(this, () => this._super(...arguments));
+  },
+
+  handlePageScroll() {
+    if (!this.get('renderInPlace') && this.get('publicAPI.isOpen')) {
+      (this.get('publicAPI.actions.reposition') || notImplementedIgnore)();
+    }
   },
 });
