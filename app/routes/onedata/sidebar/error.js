b31ed1c7953dc213451fc57eb988a45c6f96f8e3
Michał Borzęcki <michal.borzecki@hotmail.com>
2020-10-29 13:48:32 +0100

    VFS-6935 Fixed blank content page when resource access is forbidden

--- app/routes/onedata/sidebar/error.js
+++ app/routes/onedata/sidebar/error.js
@@ -2,28 +2,18 @@
  * Renders error message in content
  *
  * @module routes/onedata/sidebar/error
- * @author Jakub Liput
- * @copyright (C) 2017 ACK CYFRONET AGH
+ * @author Jakub Liput, Michał Borzęcki
+ * @copyright (C) 2017-2020 ACK CYFRONET AGH
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
 
 import Route from '@ember/routing/route';
-import { get } from '@ember/object';
 
 export default Route.extend({
-  renderTemplate(controller, model) {
-    const errorType = model && get(model, 'isOnedataCustomError') && get(model, 'type');
-    const template = errorType ? `errors/components/${errorType}` : '-internal-error';
-    try {
-      this.render(template, {
-        into: 'onedata',
-        outlet: 'content',
-      });
-    } catch (error) {
-      this.render('-internal-error', {
-        into: 'onedata',
-        outlet: 'content',
-      });
-    }
+  renderTemplate() {
+    this.render('-internal-error', {
+      into: 'onedata',
+      outlet: 'content',
+    });
   },
 });
