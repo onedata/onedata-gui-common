0861de685668576f37459bddee6b68edf0ced406
plgagneska <agawarchol15@gmail.com>
2021-01-22 16:03:20 +0100

    VFS-6915 Hide global copy button

--- addon/components/one-copy-button-global.js
+++ addon/components/one-copy-button-global.js
@@ -7,23 +7,22 @@
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
 
-import layout from '../templates/components/one-copy-button-global';
 import CopyButton from 'ember-cli-clipboard/components/copy-button';
 import { inject as service } from '@ember/service';
 import { reads } from '@ember/object/computed';
+import { computed } from '@ember/object';
 import I18n from 'onedata-gui-common/mixins/components/i18n';
 import _ from 'lodash';
 import { or } from 'ember-awesome-macros';
 import computedT from 'onedata-gui-common/utils/computed-t';
 
 export default CopyButton.extend(I18n, {
-  layout,
-
   globalClipboard: service(),
   globalNotify: service(),
   i18n: service(),
 
   /**
+   * @override
    * Use translation from one-copy-button component
    */
   i18nPrefix: 'components.oneCopyButton',
@@ -31,7 +30,7 @@ export default CopyButton.extend(I18n, {
   /**
    * @override
    */
-  class: 'btn-global-copy-button',
+  classNames: ['hidden', 'btn-global-copy-button'],
 
   /**
    * @type {Ember.ComputedProperty<string>}
@@ -43,28 +42,43 @@ export default CopyButton.extend(I18n, {
    */
   textType: or('globalClipboard.textType', computedT('defaultTextType')),
 
-  actions: {
-    clipboardSuccess() {
-      const {
-        globalNotify,
-        textType,
-      } = this.getProperties('globalNotify', 'textType');
-      globalNotify.success(this.t(
-        'copySuccess', {
-          textType: _.startCase(textType),
-        }
-      ));
-    },
-    clipboardError() {
-      const {
-        globalNotify,
-        textType,
-      } = this.getProperties('globalNotify', 'textType');
-      globalNotify.error(this.t(
-        'copyFailure', {
-          textType: textType,
-        }
-      ));
-    },
+  /**
+   * @override
+   * @type {Ember.ComputedProperty<function>}
+   */
+  success: computed(function success() {
+    return this._success.bind(this);
+  }),
+
+  /**
+   * @override
+   * @type {Ember.ComputedProperty<function>}
+   */
+  error: computed(function error() {
+    return this._error.bind(this);
+  }),
+
+  _success() {
+    const {
+      globalNotify,
+      textType,
+    } = this.getProperties('globalNotify', 'textType');
+    globalNotify.success(this.t(
+      'copySuccess', {
+        textType: _.startCase(textType),
+      }
+    ));
+  },
+
+  _error() {
+    const {
+      globalNotify,
+      textType,
+    } = this.getProperties('globalNotify', 'textType');
+    globalNotify.error(this.t(
+      'copyFailure', {
+        textType: textType,
+      }
+    ));
   },
 });
