00d528ae9477ec99e4a0f7d44ac9499e299e14b2
Jakub Liput <jakub.liput@gmail.com>
2021-01-28 15:24:03 +0100

    VFS-6566 Refactor: moved markdown-editor to oneprovider-gui

--- app/locales/en/onedata-gui-common/components/markdown-editor.js
+++
@@ -1,8 +0,0 @@
-export default {
-  discard: 'Discard',
-  save: 'Save',
-  editMarkdown: 'Edit Markdown',
-  preview: 'Preview',
-  markdownEditor: 'Markdown editor',
-  openPreview: 'Open preview',
-};
