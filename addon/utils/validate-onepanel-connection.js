70005294cb4ff09e0b9d245d6dd95a211550f7af
Jakub Liput <jakub.liput@gmail.com>
2021-02-17 10:42:10 +0100

    VFS-6638 New Onepanel validate connection check and pending animation

---
+++ addon/utils/validate-onepanel-connection.js
@@ -0,0 +1,33 @@
+/**
+ * Checks if there is a connectivity to Onepanel needed to display its GUI and if
+ * the Onepanel has the same clusterId as expected.
+ * There can be duplicated clusters with the same hostname, but with different
+ * ids. These with invalid clusterIds should be presented as dead and allow only to
+ * deregister them. 
+ *
+ * @module utils/validate-onepanel-connection
+ * @author Jakub Liput
+ * @copyright (C) 2021 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ */
+
+import $ from 'jquery';
+import { resolve } from 'rsvp';
+
+export default function validateOnepanelConnection(clusterOrigin, clusterId) {
+  return resolve($.get(`${clusterOrigin}:9443/configuration`))
+    .then(({ clusterId: fetchedClusterId }) => {
+      if (fetchedClusterId) {
+        if (fetchedClusterId === clusterId) {
+          return true;
+        } else {
+          console.error('util:validate-onepanel-connection: cluster id does not match');
+          return false;
+        }
+      } else {
+        console.error('util:validate-onepanel-connection: no cluster id available');
+        return false;
+      }
+    })
+    .catch(() => false);
+}
