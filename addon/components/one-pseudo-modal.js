7029612be40d91d7bc4130c0533e7bdfb93f677c
Jakub Liput <jakub.liput@gmail.com>
2021-03-10 19:18:43 +0100

    VFS-7403 Experimental pseudo-modal for faster testing

---
+++ addon/components/one-pseudo-modal.js
@@ -0,0 +1,7 @@
+import Component from '@ember/component';
+import layout from '../templates/components/one-pseudo-modal';
+
+export default Component.extend({
+  layout,
+  classNames: ['one-pseudo-modal', 'modal', 'in'],
+});
