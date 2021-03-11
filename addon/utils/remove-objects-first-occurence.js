a9e4818aeab0346603d39843d1ff03f7a9cfd003
Jakub Liput <jakub.liput@gmail.com>
2021-03-11 14:33:41 +0100

    VFS-7368 PR fixes

--- addon/utils/remove-objects-first-occurence.js
+++ addon/utils/remove-objects-first-occurence.js
@@ -14,7 +14,7 @@
  * 
  * @module utils/remove-objects-first-occurence
  * @author Jakub Liput
- * @copyright (C) 2019-2020 ACK CYFRONET AGH
+ * @copyright (C) 2021 ACK CYFRONET AGH
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
 
