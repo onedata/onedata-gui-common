db0a3e45b43b55bea622259ee4221348459f20e4
Jakub Liput <jakub.liput@gmail.com>
2021-03-15 17:42:37 +0100

    VFS-7403 Refactor

--- addon/components/one-element.js
+++ addon/components/one-element.js
@@ -1,3 +1,14 @@
+/**
+ * Renders just a HTML element.
+ * 
+ * Can be used for example for yielding a simple element as a component from template.
+ *
+ * @module components/one-element
+ * @author Jakub Liput
+ * @copyright (C) 2021 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ */
+
 import Component from '@ember/component';
 import layout from '../templates/components/one-element';
 
