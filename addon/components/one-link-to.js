5bfbf73bf882cde6b4324a7b603368f165920105
Michał Borzęcki <michal.borzecki@hotmail.com>
2021-03-22 09:53:09 +0100

    VFS-7470 Added Enter handling

--- addon/components/one-link-to.js
+++ addon/components/one-link-to.js
@@ -10,12 +10,46 @@
  */
 
 import LinkComponent from '@ember/routing/link-component';
+import { inject as service } from '@ember/service';
+import { equal, raw } from 'ember-awesome-macros';
 
 export default LinkComponent.extend({
   attributeBindings: ['draggable'],
 
+  browser: service(),
+
   /**
    * @type {Boolean}
    */
-  draggable: false,
+  draggable: undefined,
+
+  /**
+   * @type {ComputedProperty<Boolean>}
+   */
+  isInFirefox: equal('browser.browser.browserCode', raw('firefox')),
+
+  init() {
+    this._super(...arguments);
+
+    const {
+      isInFirefox,
+      draggable,
+    } = this.getProperties('isInFirefox', 'draggable');
+    if (draggable === undefined && isInFirefox) {
+      this.set('draggable', false);
+    }
+  },
+
+  keyDown(event) {
+    if (
+      this.get('isInFirefox') &&
+      event.key === 'Enter' &&
+      event.target.tagName === 'INPUT'
+    ) {
+      event.preventDefault();
+      return;
+    }
+
+    return this._super(...arguments);
+  },
 });
