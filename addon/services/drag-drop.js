61c47e7b58df58914810c363ae594171656c049a
Michał Borzęcki <michal.borzecki@hotmail.com>
2021-04-15 15:16:03 +0200

    VFS-7330 PR fixes

---
+++ addon/services/drag-drop.js
@@ -0,0 +1,24 @@
+/**
+ * Hides external implemetation of ember-drag-drop dragCoordinator service behind
+ * our API.
+ *
+ * @module services/drag-drop
+ * @author Michał Borzęcki
+ * @copyright (C) 2021 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ */
+
+import Service, { inject as service } from '@ember/service';
+import { getBy } from 'ember-awesome-macros';
+
+export default Service.extend({
+  dragCoordinator: service(),
+
+  /**
+   * @type {ComputedProperty<any>}
+   */
+  draggedElementModel: getBy(
+    'dragCoordinator.currentDragObject',
+    'dragCoordinator.currentDragObject.unwrappingKey'
+  ),
+});
