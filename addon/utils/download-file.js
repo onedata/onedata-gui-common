45781a9fecea9520839d4b87c4826b2189d83430
Michał Borzęcki <michal.borzecki@hotmail.com>
2021-06-29 15:08:01 +0200

    VFS-7818 Extracted file downloading logic to util

---
+++ addon/utils/download-file.js
@@ -0,0 +1,42 @@
+/**
+ * Downloads file using given URL.
+ *
+ * @module util/download-file
+ * @author Jakub Liput, Michał Borzęcki
+ * @copyright (C) 2021 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ */
+
+import $ from 'jquery';
+import { get } from '@ember/object';
+
+export default function downloadFile({ fileUrl, isMobileService, _window }) {
+  const isMobileBrowser = get(isMobileService, 'any');
+  if (!fileUrl) {
+    return;
+  }
+
+  if (isMobileBrowser) {
+    downloadUsingOpen(fileUrl, isMobileService, _window);
+  } else {
+    downloadUsingIframe(fileUrl, _window);
+  }
+}
+
+function downloadUsingIframe(fileUrl, _window) {
+  const body = _window.document.body;
+  const iframe = $('<iframe/>').attr({
+    src: fileUrl,
+    style: 'display:none;',
+  }).appendTo(body);
+  // the time should be long to support some download extensions in Firefox desktop
+  setTimeout(() => iframe.remove(), 60000);
+}
+
+function downloadUsingOpen(fileUrl, isMobileService, _window) {
+  // Apple devices such as iPad tries to open file using its embedded viewer
+  // in any browser, but we cannot say if the file extension is currently supported
+  // so we try to open every file in new tab.
+  const target = get(isMobileService, 'apple.device') ? '_blank' : '_self';
+  _window.open(fileUrl, target);
+}
