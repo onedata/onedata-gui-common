7e8dbeb036aa95b72a17ee99c191e82e85193984
Jakub Liput <jakub.liput@gmail.com>
2021-08-09 19:37:27 +0200

    VFS-7633 Sleep util now uses Ember later

--- addon/utils/sleep.js
+++ addon/utils/sleep.js
@@ -3,12 +3,13 @@
  * 
  * @module utils/sleep
  * @author Jakub Liput
- * @copyright (C) 2020 ACK CYFRONET AGH
+ * @copyright (C) 2020-2021 ACK CYFRONET AGH
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
 
 import { Promise } from 'rsvp';
+import { later } from '@ember/runloop';
 
 export default async function sleep(timeout) {
-  await new Promise(resolve => setTimeout(resolve, timeout));
+  await new Promise(resolve => later(resolve, timeout));
 }
