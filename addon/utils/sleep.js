212cee236a3200974934ec3e90aafee3a48184c9
Jakub Liput <jakub.liput@gmail.com>
2021-08-10 09:36:10 +0200

    VFS-7633 Using setTimeout in sleep in test mode

--- addon/utils/sleep.js
+++ addon/utils/sleep.js
@@ -7,9 +7,11 @@
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
 
+import config from 'ember-get-config';
 import { Promise } from 'rsvp';
 import { later } from '@ember/runloop';
 
 export default async function sleep(timeout) {
-  await new Promise(resolve => later(resolve, timeout));
+  const scheduleFun = (config.environment === 'test') ? setTimeout : later;
+  await new Promise(resolve => scheduleFun(resolve, timeout));
 }
