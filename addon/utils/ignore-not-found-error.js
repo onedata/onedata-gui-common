95e6e68797b8758e81ace9a48cab0feb1d5e40a9
Michał Borzęcki <michal.borzecki@hotmail.com>
2021-08-17 12:48:56 +0200

    VFS-7900 Added ignoreNotFoundError util

---
+++ addon/utils/ignore-not-found-error.js
@@ -0,0 +1,20 @@
+/**
+ * Util function that can be used to ignore `notFound` errors received
+ * in server responses. If error is different than forbidden, it is
+ * rethrown.
+ *
+ * @module utils/ignore-not-found-error
+ * @author Michał Borzęcki
+ * @copyright (C) 2021 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ *
+ * @param {*} result response
+ * @returns {*}
+ */
+export default function ignoreNotFoundError(result) {
+  if (result && result.id !== 'notFound') {
+    throw result;
+  } else {
+    return result;
+  }
+}
