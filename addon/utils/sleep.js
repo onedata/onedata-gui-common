50b9f048e1ad431d316ec845e331b4df5e210afd
Jakub Liput <jakub.liput@gmail.com>
2021-08-18 10:02:44 +0200

    VFS-7633 PR fixes: using setTimeout in all environments in sleep util

--- addon/utils/sleep.js
+++ addon/utils/sleep.js
@@ -7,11 +7,8 @@
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
 
-import config from 'ember-get-config';
 import { Promise } from 'rsvp';
-import { later } from '@ember/runloop';
 
 export default async function sleep(timeout) {
-  const scheduleFun = (config.environment === 'test') ? setTimeout : later;
-  await new Promise(resolve => scheduleFun(resolve, timeout));
+  await new Promise(resolve => setTimeout(resolve, timeout));
 }
