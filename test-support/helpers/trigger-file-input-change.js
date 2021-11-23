d6dceac4b3aeb6159256fd9ac103d625f7be36f6
Michał Borzęcki <m.borzecki@cyfronet.pl>
2021-11-23 15:59:51 +0100

    VFS-8255 Added test helper trigger-file-input-change

---
+++ test-support/helpers/trigger-file-input-change.js
@@ -0,0 +1,17 @@
+import { triggerEvent } from 'ember-native-dom-helpers';
+
+/**
+ *
+ * @param {HTMLInputElement} input
+ * @param {Array<{ name: string, content: string, mimeType: string }>} files
+ */
+export default async function triggerFileInputChange(input, files) {
+  const dataTransfer = new DataTransfer();
+  files.forEach(({ name, content, mimeType }) => {
+    const contentBlob = new Blob([content], { type: mimeType });
+    const fileObject = new File([contentBlob], name, { type: mimeType });
+    dataTransfer.items.add(fileObject);
+  });
+  input.files = dataTransfer.files;
+  await triggerEvent(input, 'change');
+}
