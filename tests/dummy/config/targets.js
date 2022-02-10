a97565f1286b08b2eac3c094fb6d77b932ce2d40
Jakub Liput <jakub.liput@gmail.com>
2022-02-10 14:08:45 +0100

    VFS-7717 Updated targets

--- tests/dummy/config/targets.js
+++ tests/dummy/config/targets.js
@@ -1,9 +1,9 @@
 /* eslint-env node */
 module.exports = {
   browsers: [
-    'ie 10',
-    'last 1 Chrome versions',
-    'last 1 Firefox versions',
-    'last 1 Safari versions',
+    'since 2017',
+    // must be specified, due to potential bug in build chain: only old versions
+    // of android are listed in Babel's data, so it will always add plugins for Android 4
+    'not android > 4',
   ],
 };
