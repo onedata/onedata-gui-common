c074d9b4117094c2d769ddca843c582303f81f10
Michał Borzęcki <m.borzecki@cyfronet.pl>
2022-03-09 17:57:51 +0100

    VFS-9130 Hiding passed tests

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,7 +1,6 @@
 import resolver from './helpers/resolver';
 import './helpers/responsive';
-import { mocha } from 'mocha';
-
+import { mocha, afterEach } from 'mocha';
 import { setResolver } from 'ember-mocha';
 
 setResolver(resolver);
@@ -9,3 +8,12 @@ setResolver(resolver);
 mocha.setup({
   timeout: 5000,
 });
+
+const urlParams = new URLSearchParams(location.search);
+if (urlParams.get('hidepassed') !== null) {
+  afterEach(function () {
+    if (this.currentTest.state === 'passed') {
+      document.querySelectorAll('.test.pass').forEach(node => node.remove());
+    }
+  });
+}
