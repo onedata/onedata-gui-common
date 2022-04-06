2be5bb6ea511030fd5f58420d3cd3f2cf2ccd7ef
Jakub Liput <jakub.liput@gmail.com>
2022-04-06 15:30:27 +0200

    VFS-7135 Added hidepassed handler to common

---
+++ test-support/handle-hidepassed.js
@@ -0,0 +1,11 @@
+export default function handleHidepassed(afterEach) {
+  // Remove all passed test reports from DOM if `hidepassed` query param is present
+  const urlParams = new URLSearchParams(location.search);
+  if (urlParams.get('hidepassed') !== null) {
+    afterEach(function () {
+      if (this.currentTest.state === 'passed') {
+        document.querySelectorAll('.test.pass').forEach(node => node.remove());
+      }
+    });
+  }
+}
