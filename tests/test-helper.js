9c3fe374fc8d1ffe3829ac7ed74110984ac6da84
Michał Borzęcki <m.borzecki@cyfronet.pl>
2022-04-21 11:49:40 +0200

    VFS-9093 Upgraded ember-responsive

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,7 +1,9 @@
 import { start, setResolver } from 'ember-mocha';
 import { mocha, afterEach } from 'mocha';
+import { setApplication } from '@ember/test-helpers';
 import resolver from './helpers/resolver';
-import './helpers/responsive';
+import Application from '../app';
+import config from '../config/environment';
 import { unsuppressRejections } from './helpers/suppress-rejections';
 import handleHidepassed from './handle-hidepassed';
 
@@ -9,6 +11,9 @@ mocha.setup({
   timeout: 5000,
 });
 setResolver(resolver);
+setApplication(Application.create(
+  Object.assign({}, config.APP, { autoboot: false })
+));
 
 afterEach(unsuppressRejections);
 
