b29899618408189359ac571c55c5b4d2467dfba1
Michał Borzęcki <m.borzecki@cyfronet.pl>
2022-06-21 17:06:58 +0200

    VFS-8716 Added values converting functions to dataspec editor

--- addon/utils/atm-workflow/data-spec-editor/data-spec-editor2.js
+++ addon/utils/atm-workflow/data-spec-editor/data-spec-editor2.js
@@ -1,5 +1,9 @@
 import FormField from 'onedata-gui-common/utils/form-component/form-field';
-import generateId from 'onedata-gui-common/utils/generate-id';
+import {
+  createDataTypeSelectorElement,
+  createDataTypeElement,
+} from './create-data-spec-editor-element';
+import valueConstraintsEditors from './value-constraints-editors';
 
 export const FormElement = FormField.extend({
   /**
@@ -14,10 +18,56 @@ export const FormElement = FormField.extend({
 });
 
 export function dataSpecToFormValues(dataSpec) {
-  if (!dataSpec) {
+  if (!dataSpec || !dataSpec.type) {
+    return createDataTypeSelectorElement();
+  }
+
+  const dataType = dataSpec.type;
+  const valueConstraints = dataSpec.valueConstraints || {};
+
+  if (dataType in valueConstraintsEditors) {
+    return createDataTypeElement(dataType, {
+      formValues: valueConstraintsEditors[dataType]
+        .valueConstraintsToFormValues(valueConstraints),
+    });
+  } else if (dataType === 'array') {
+    return createDataTypeElement(dataType, {
+      item: dataSpecToFormValues(valueConstraints.itemDataSpec),
+    });
+  } else {
+    return createDataTypeElement(dataType);
+  }
+}
+
+export function formValuesToDataSpec(values) {
+  if (
+    !values ||
+    !values.type !== 'dataType' ||
+    !values.config ||
+    !values.config.dataType
+  ) {
+    return null;
+  }
+
+  const dataType = values.config.dataType;
+
+  if (dataType in valueConstraintsEditors) {
     return {
-      id: generateId(),
-      type: 'dataTypeSelector',
+      type: dataType,
+      valueConstraints: valueConstraintsEditors[dataType]
+        .formValuesToValueConstraints(values.config.formValues),
+    };
+  } else if (dataType === 'array') {
+    return {
+      type: dataType,
+      valueConstraints: {
+        itemDataSpec: formValuesToDataSpec(values.config.item),
+      },
+    };
+  } else {
+    return {
+      type: dataType,
+      valueConstraints: {},
     };
   }
 }
