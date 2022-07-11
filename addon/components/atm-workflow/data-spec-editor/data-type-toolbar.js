c4d6c54f9d3c6903ae9565487044c29cd696be58
Michał Borzęcki <m.borzecki@cyfronet.pl>
2022-07-11 17:34:50 +0200

    VFS-8716 Added icons dedicated for dataspec editor

--- addon/components/atm-workflow/data-spec-editor/data-type-toolbar.js
+++ addon/components/atm-workflow/data-spec-editor/data-type-toolbar.js
@@ -14,6 +14,7 @@ import layout from '../../../templates/components/atm-workflow/data-spec-editor/
 export default Component.extend(I18n, {
   layout,
   classNames: ['data-type-toolbar'],
+  classNameBindings: ['isEnabled::disabled'],
 
   i18n: service(),
 
