114a44dda0c674dc003c5efdbf384a50499eebcc
Michał Borzęcki <m.borzecki@cyfronet.pl>
2022-07-12 11:15:25 +0200

    VFS-8716 Added tooltips to dataspec editor toolbar

--- app/locales/en/onedata-gui-common/components/atm-workflow/data-spec-editor/data-type-toolbar.js
+++ app/locales/en/onedata-gui-common/components/atm-workflow/data-spec-editor/data-type-toolbar.js
@@ -1,4 +1,9 @@
 export default {
+  actions: {
+    unpackFromArray: 'Flatten the array',
+    packIntoArray: 'Wrap into an array',
+    remove: 'Remove',
+  },
   removeWarn: {
     content: '<strong>Warning:</strong> removing this data type specification will result in removing it\'s configuration and/or nested data types.',
     yes: 'Remove',
