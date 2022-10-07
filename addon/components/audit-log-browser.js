e6b89601f7df71a6dc76564e536c0cc43b38c776
Jakub Liput <jakub.liput@gmail.com>
2022-10-07 08:48:59 +0200

    VFS-9614 Customizable log-entry-details component

--- addon/components/audit-log-browser.js
+++ addon/components/audit-log-browser.js
@@ -111,6 +111,14 @@ export default Component.extend(I18n, {
   onFetchLogEntries: undefined,
 
   /**
+   * Name of component to render as `log-entry-details` sliding panel.
+   * By default it uses component with raw log entry presenter.
+   * @virtual optional
+   * @type {string}
+   */
+  logEntryDetailsComponentName: 'audit-log-browser/log-entry-details',
+
+  /**
    * Returns class names for each log entry. These classes are then applied to
    * the corresponding table row.
    * @virtual optional
@@ -176,6 +184,18 @@ export default Component.extend(I18n, {
    * @type {boolean}
    */
   doesOpenDetailsOnClick: false,
+
+  /**
+   * Extra data provided to configure default log-entry-details view.
+   * May be overriden if custom log entry details component is used.
+   * @type {any}
+   */
+  logEntryDetailsModel: computed(
+    'isTimestampRoundedToSeconds',
+    function logEntryDetailsModel() {
+      return { isTimestampRoundedToSeconds: this.isTimestampRoundedToSeconds };
+    }
+  ),
 
   /**
    * `onFetchLogEntries` adjusted to be compatible with infinite-scroll-table
