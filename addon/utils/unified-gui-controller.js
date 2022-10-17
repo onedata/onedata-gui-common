d261c5daf5bfcb29ec9876d88c44db9dc3d5fe50
Jakub Liput <jakub.liput@gmail.com>
2022-10-17 12:37:46 +0200

    VFS-9926 Fixed setAsOpened

--- addon/utils/unified-gui-controller.js
+++ addon/utils/unified-gui-controller.js
@@ -24,7 +24,7 @@ export default Object.freeze({
    * @return {void}
    */
   setAsOpened() {
-    this.setNonIframeRedirectFlag(true);
+    this.setNonIframeRedirectFlag(false);
   },
 
   isInIframe() {
