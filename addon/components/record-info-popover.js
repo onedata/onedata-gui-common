a9a8e3dd1dcdfe9fe773ea5751bb52da6dc7c038
plgagneska <agawarchol15@gmail.com>
2022-11-16 16:17:57 +0100

    VFS-9997 Change popover info

---
+++ addon/components/record-info-popover.js
@@ -0,0 +1,13 @@
+/**
+ * Renders record info popover.
+ *
+ * @author Agnieszka Warchoł
+ * @copyright (C) 2022 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ */
+
+import Component from '@ember/component';
+
+export default Component.extend({
+  classNames: ['record-info-popover'],
+});
