7296652f28ace7f56004aa27730c146fe794517e
plgagneska <agawarchol15@gmail.com>
2022-12-08 09:51:33 +0100

    VFS-10130 Resolve PR tasks

--- addon/components/modals/toggle-dir-stats-question-modal.js
+++ addon/components/modals/toggle-dir-stats-question-modal.js
@@ -1,12 +1,14 @@
 /**
- * A modal that allows enable/disable or cancel collect directory statistics
+ * A modal that allows enable/disable or cancel collect directory statistics. 
+ * Data needed from modalOptions:
+ * - nextDirStatsEnabledValue - state of directory statistics after accept this modal. 
+ *   One of `enabled`, `disabled`.
  *
  * @author Agnieszka Warchoł
  * @copyright (C) 2022 ACK CYFRONET AGH
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
 
-import computedT from 'onedata-gui-common/utils/computed-t';
 import { reads } from '@ember/object/computed';
 import QuestionModal from './question-modal';
 import { computed } from '@ember/object';
@@ -29,12 +31,14 @@ export default QuestionModal.extend({
   /**
    * @override
    */
-  headerText: computedT('header'),
+  headerText: computed('i18nPrefix', function headerText() {
+    return this.t('header');
+  }),
 
   /**
    * @override
    */
-  descriptionParagraphs: computed(function descriptionParagraphs() {
+  descriptionParagraphs: computed('i18nPrefix', function descriptionParagraphs() {
     return [{
       text: this.t('question'),
     }, {
@@ -45,7 +49,9 @@ export default QuestionModal.extend({
   /**
    * @override
    */
-  yesButtonText: computedT('buttonConfirm'),
+  yesButtonText: computed('i18nPrefix', function yesButtonText() {
+    return this.t('buttonConfirm');
+  }),
 
   /**
    * @override
@@ -57,7 +63,7 @@ export default QuestionModal.extend({
   ),
 
   /**
-   * @type {ComputedProperty<String>}
+   * @type {ComputedProperty<'enabled'|'disabled'>}
    */
   nextDirStatsEnabledValue: reads('modalOptions.nextDirStatsEnabledValue'),
 });
