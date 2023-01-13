9c724050764100bfe4a8920f6ce1637a1e6abc21
Jakub Liput <jakub.liput@gmail.com>
2023-01-13 16:45:40 +0100

    VFS-10114 Removed routeWillChange workaround

--- addon/utils/onedata-router.js
+++ addon/utils/onedata-router.js
@@ -1,9 +1,6 @@
 /**
  * Custom Onedata router extension.
  *
- * Contains events feed needed to backport `routeWillChange` event functionality.
- * TODO: VFS-8267 Remove `routeWillChange` event backport on Ember upgrade.
- *
  * @module utils/onedata-router
  * @author Michał Borzęcki
  * @copyright (C) 2020-2021 ACK CYFRONET AGH
@@ -18,16 +15,7 @@ export default EmberRouter.extend({
 
   // Clear all query params on each transition
   // WARNING: This method belongs to a private Ember API. May change in next Ember versions
-  _hydrateUnsuppliedQueryParams() {},
-
-  init() {
-    this._super(...arguments);
-
-    this.on('willTransition', (transition) => {
-      const router = this.get('router');
-      if (typeof router.trigger === 'function') {
-        router.trigger('routeWillChange', transition);
-      }
-    });
+  _hydrateUnsuppliedQueryParams() {
+    // FIXME: check it works in 3.8
   },
 });
