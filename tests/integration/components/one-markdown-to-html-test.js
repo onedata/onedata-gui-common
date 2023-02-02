de1516b98a2c6d55a286d1f42f1fafc6bd6bf77d
Jakub Liput <jakub.liput@gmail.com>
2023-02-02 13:39:27 +0100

    VFS-10481 Fixed imports in tests

--- tests/integration/components/one-markdown-to-html-test.js
+++ tests/integration/components/one-markdown-to-html-test.js
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import { describe, it, beforeEach, afterEach } from 'mocha';
 import { setupRenderingTest } from 'ember-mocha';
 import { render, click } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
+import { hbs } from 'ember-cli-htmlbars';
 
 describe('Integration | Component | one markdown to html', function () {
   setupRenderingTest();
