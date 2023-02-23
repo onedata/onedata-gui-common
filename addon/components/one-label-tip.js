56f149fa2b52736eed7254c44690f0aaf0c7ad8d
Michał Borzęcki <m.borzecki@cyfronet.pl>
2023-02-23 10:10:00 +0100

    VFS-9688 Removed arrow placement from one-tooltip

--- addon/components/one-label-tip.js
+++ addon/components/one-label-tip.js
@@ -1,10 +1,10 @@
 /**
  * Inserts 'help' icon with tooltip
- * Typical usage: 
+ * Typical usage:
  * ```
  * {{one-label-tip title="tooltip text"}}
  * ```
- * 
+ *
  * @module components/one-label-tip
  * @author Michal Borzecki
  * @copyright (C) 2017-2020 ACK CYFRONET AGH
@@ -39,16 +39,10 @@ export default Component.extend({
   icon: 'sign-question-rounded',
 
   /**
-   * Placement of the tooltip 
+   * Placement of the tooltip
    * @type {string}
    */
   placement: 'top',
-
-  /**
-   * Placement of the tooltip arrow
-   * @type {string}
-   */
-  arrowPlacement: 'right',
 
   /**
    * The event(s) that should trigger the tooltip
@@ -60,7 +54,7 @@ export default Component.extend({
 
   /**
    * In mobile mode, it's better to not propagate click event from tip.
-   * @param {Event} clickEvent 
+   * @param {Event} clickEvent
    */
   click(clickEvent) {
     clickEvent.preventDefault();
