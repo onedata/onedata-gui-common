fc768f1364431a791e832838e0a0f4adee0be565
plgagneska <agawarchol15@gmail.com>
2023-03-01 15:40:05 +0100

    VFS-10372 Change classes in harvester info content

--- addon/components/harvester-info-content.js
+++ addon/components/harvester-info-content.js
@@ -5,6 +5,7 @@
  * @copyright (C) 2023 ACK CYFRONET AGH
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
+
 import Component from '@ember/component';
 import I18n from 'onedata-gui-common/mixins/components/i18n';
 import { inject as service } from '@ember/service';
