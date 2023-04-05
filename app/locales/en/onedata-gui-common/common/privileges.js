6c04126f0e634939c46afee82f27c8eb9714e1e8
Jakub Liput <jakub.liput@gmail.com>
2023-04-05 12:50:24 +0200

    VFS-10252 Added space_manage_marketplace privilege

--- app/locales/en/onedata-gui-common/common/privileges.js
+++ app/locales/en/onedata-gui-common/common/privileges.js
@@ -5,6 +5,7 @@ export default {
     space_delete: 'remove space',
     space_view_privileges: 'view privileges',
     space_set_privileges: 'set privileges',
+    space_manage_marketplace: 'manage Marketplace',
 
     space_read_data: 'read files',
     space_write_data: 'write files',
