35bc3967069dbf263108adc07dbd26a0f5a17487
Jakub Liput <jakub.liput@gmail.com>
2023-06-16 15:49:54 +0200

    VFS-10962 Moved websocket-reconnection modal to onedata-gui-common

---
+++ app/locales/en/onedata-gui-common/components/websocket-reconnection-modal.js
@@ -0,0 +1,19 @@
+export default {
+  header: 'WebSocket connection lost',
+  textWaitingIntro: 'The connection to Onezone server has been lost.',
+  textWaitingAttempting: 'Attempting to estabilish WebSocket connection',
+  textWaitingTry: '(try {{currentAttempt}} of {{maxAttempts}})',
+  textWaitingTimeout: 'Automatic reconnection failed – try to connect with the button below or reload the page.',
+  textNextAttempt: 'Next attempt in',
+  seconds: 'seconds',
+  second: 'second',
+  connectionAttemptManual: 'Trying to connect...',
+  tryToConnectNow: 'Try to connect now',
+  reconnecting: 'Reconnecting...',
+  reload: 'Reload the page',
+  connectedSuccessfully: 'Connection restored successfully',
+  connectionFailed: 'Connection failed',
+  specialText: {
+    safariCert: 'It may be caused by either server unavailability or invalid SSL certificate (eg. it\'s self-signed).',
+  },
+};
