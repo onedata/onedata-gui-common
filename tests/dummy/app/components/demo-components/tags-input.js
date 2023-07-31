54c469bfc1be5c1687881ed21b1c853404abd7f5
Jakub Liput <jakub.liput@gmail.com>
2023-07-31 18:26:16 +0200

    VFS-10664  Added readonlyTagsDisplayLimit feature to tags-input

--- tests/dummy/app/components/demo-components/tags-input.js
+++ tests/dummy/app/components/demo-components/tags-input.js
@@ -1,11 +1,12 @@
 /**
  * @author Michał Borzęcki
- * @copyright (C) 2019 ACK CYFRONET AGH
+ * @copyright (C) 2019-2023 ACK CYFRONET AGH
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
 
 import Component from '@ember/component';
 import { computed } from '@ember/object';
+import _ from 'lodash';
 
 export default Component.extend({
   tags: computed(function tags() {
@@ -21,6 +22,10 @@ export default Component.extend({
       label: 'tag4',
       icon: 'user',
     }];
+  }),
+
+  manyTags: computed(function manyTags() {
+    return _.range(10).map(i => ({ icon: 'browser-attribute', label: `tag-${i}` }));
   }),
 
   selectorSettings: computed('tags', function selectorSettings() {
