d1811017ee6d5ee8b3a16e817ca41f89813d36b7
Jakub Liput <jakub.liput@gmail.com>
2024-02-26 11:00:13 +0100

    VFS-11710 Refactored isCrossOriginIframe util

--- addon/utils/is-cross-origin-iframe.js
+++ addon/utils/is-cross-origin-iframe.js
@@ -10,14 +10,10 @@
 import globals from 'onedata-gui-common/utils/globals';
 
 export default function isCrossOriginIframe() {
-  let parentOrigin;
   try {
-    parentOrigin = globals.window.parent.location.origin;
+    return globals.window.location.origin !== globals.window.parent.location.origin;
   } catch {
     // getting parent origin means that it is blocked for security reasons
-  }
-  if (!parentOrigin) {
     return true;
   }
-  return globals.window.location.origin !== parentOrigin;
 }
