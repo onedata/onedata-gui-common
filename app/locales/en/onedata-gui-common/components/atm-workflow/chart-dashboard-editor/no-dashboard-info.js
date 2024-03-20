ea52faca36e5ea548b4da815e3be2789177ff3c7
Michał Borzęcki <m.borzecki@cyfronet.pl>
2024-03-20 09:18:30 +0100

    VFS-11264 PR fixes

--- app/locales/en/onedata-gui-common/components/atm-workflow/chart-dashboard-editor/no-dashboard-info.js
+++ app/locales/en/onedata-gui-common/components/atm-workflow/chart-dashboard-editor/no-dashboard-info.js
@@ -1,5 +1,6 @@
 export default {
   header: 'Chart dashboard is not defined yet.',
+  headerTip: 'A dashboard is a collection of charts organized into sections. Each chart can visualize the time series collected during a workflow execution. Such a presentation will help users track the progress of data processing and make workflows easier to understand.',
   description: 'To create one, click the button below.',
   createButton: 'Create dashboard',
 };
