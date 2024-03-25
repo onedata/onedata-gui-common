579978ff6aaab7bd91d6fa53885a11db38ccc0d2
Michał Borzęcki <m.borzecki@cyfronet.pl>
2024-03-25 11:29:35 +0100

    VFS-11845 Fixed crashing ember serve after build failure

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -11,6 +11,17 @@ const dynamicLibraries = require('./config/dynamic-libraries');
 const sass = require('sass-embedded');
 
 module.exports = function (defaults) {
+  // BUGFIX: Fixes stopping `ember serve` after any build error. Fix based on
+  // ember-cli issue https://github.com/ember-cli/ember-cli/issues/9404.
+  // It's already fixed by https://github.com/ember-cli/ember-cli/pull/9987
+  // in the 4.8 ember-cli release
+  // https://github.com/ember-cli/ember-cli/releases/tag/v4.8.0.
+  // TODO: VFS-11893 Remove this hack.
+  process.on('uncaughtException', function (error) {
+    console.error(error.stack);
+    console.log('Not allowing node to exit after build error.');
+  });
+
   const app = new EmberAddon(defaults, {
     'fingerprint': {
       extensions: [
