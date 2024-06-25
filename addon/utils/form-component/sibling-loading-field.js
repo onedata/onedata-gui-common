bc73c818b5abba329724bb4f02b2ee7566d41776
Jakub Liput <jakub.liput@gmail.com>
2024-06-25 10:43:47 +0200

    VFS-11889 Fixed issues with sibling-loading-field

--- addon/utils/form-component/sibling-loading-field.js
+++ addon/utils/form-component/sibling-loading-field.js
@@ -7,7 +7,7 @@
  */
 
 import LoadingField from 'onedata-gui-common/utils/form-component/loading-field';
-import { computed } from '@ember/object';
+import { computed, defineProperty, observer } from '@ember/object';
 import { reads, not } from '@ember/object/computed';
 
 export default LoadingField.extend({
@@ -25,19 +25,61 @@ export default LoadingField.extend({
   /**
    * @override
    */
-  label: computed('parent.fields.@each.{name,label}', 'siblingName', function label() {
-    return this.parent?.fields.find(({ name }) => name === this.siblingName)?.label;
-  }),
-
-  /**
-   * @override
-   */
-  tip: computed('parent.fields.@each.{name,tip}', 'siblingName', function tip() {
-    return this.parent?.fields.find(({ name }) => name === this.siblingName)?.tip;
-  }),
-
-  /**
-   * @override
-   */
   isValid: reads('isFulfilled'),
+
+  /**
+   * @type {ComputedProperty<number>}
+   */
+  siblingIndexInParent: computed(
+    'parent.fields.@each.name',
+    function siblingIndexInParent() {
+      return this.parent?.fields.findIndex(field =>
+        field.name === this.siblingName
+      ) ?? -1;
+    }
+  ),
+
+  /**
+   * Define `reads` for sibling's tip every time the parent fields change.
+   * @type {Ember.Observer}
+   */
+  siblingTipSetter: observer(
+    'siblingIndexInParent',
+    function siblingTipSetter() {
+      const siblingIndex = this.siblingIndexInParent;
+      if (siblingIndex === -1) {
+        defineProperty(this, 'tip', {
+          value: undefined,
+          configurable: true,
+        });
+      } else {
+        defineProperty(this, 'tip', reads('parent.fields.${siblingIndex}.tip'));
+      }
+    }
+  ),
+
+  /**
+   * Define `reads` for sibling's label every time the parent fields change.
+   * @type {Ember.Observer}
+   */
+  siblingLabelSetter: observer(
+    'siblingIndexInParent',
+    function siblingLabelSetter() {
+      const siblingIndex = this.siblingIndexInParent;
+      if (siblingIndex === -1) {
+        defineProperty(this, 'label', {
+          value: undefined,
+          configurable: true,
+        });
+      } else {
+        defineProperty(this, 'label', reads('parent.fields.${siblingIndex}.label'));
+      }
+    }
+  ),
+
+  init() {
+    this._super(...arguments);
+    this.siblingLabelSetter();
+    this.siblingTipSetter();
+  },
 });
