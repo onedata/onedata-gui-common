c31cb3d5824f65129036a817ee185a2f3b133863
Jakub Liput <jakub.liput@gmail.com>
2024-06-25 10:43:30 +0200

    VFS-11889 Added install-get-by-property util

--- addon/utils/install-get-by-property.js
+++ addon/utils/install-get-by-property.js
@@ -1,28 +1,53 @@
-import { observer, defineProperty } from '@ember/object';
+/**
+ * Adds a property with `newPropertyName` to the `emberObject` which will read a value
+ * from the object using the `sourceObjectKey` from the `emberObject`.
+ *
+ * The value is evaluated by key using the `targetPath`, for example:
+ *
+ * ```
+ * const emberObject = EmberObject.create({
+ *   someObject: { foo: 1, bar: 2 },
+ *   someProperty: 'foo',
+ * });
+ * installGetByProperty(emberObject, 'hello', 'someObject', 'someProperty');
+ * ```
+ *
+ * will result adding a `hello` property to the emberObject that will be a `reads` to
+ * `someObject.foo`, thus it will have an actual value of `1`.
+ *
+ * The `hello` property will be automatically updated to read the value of the other
+ * property of `someObject` if `someProperty` will be changed.
+ *
+ * Note: this util adds an observer to the object, so remember to destroy the target
+ * object.
+ *
+ *
+ * @author Jakub Liput
+ * @copyright (C) 2024 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ */
+
+import { defineProperty } from '@ember/object';
 import { reads } from '@ember/object/computed';
 
 export default function installGetByProperty(
   emberObject,
   newPropertyName,
   sourceObjectKey,
-  targetPropertyPath
+  targetPath
 ) {
-  const definitionObserver = observer(
-    'sourceObjectKey',
-    'targetPropertyPath',
-    function defineDefaultValue() {
-      defineProperty(
-        this,
-        newPropertyName,
-        reads(`${sourceObjectKey}.${targetPropertyPath}`)
-      );
-    }
-  );
-  const observerName = `__${newPropertyName}DefinitionObserver`;
-  defineProperty(
-    emberObject,
-    observerName,
-    definitionObserver
-  );
-  emberObject[observerName]();
+  const redefineFunctionName = `__${newPropertyName}Redefine`;
+  const redefineProperty = function redefineProperty() {
+    const path = this[targetPath];
+    defineProperty(
+      this,
+      newPropertyName,
+      reads(`${sourceObjectKey}.${path}`)
+    );
+  };
+  emberObject[redefineFunctionName] = redefineProperty;
+  emberObject[redefineFunctionName]();
+  emberObject.addObserver(sourceObjectKey, emberObject, redefineFunctionName);
+  emberObject.addObserver(targetPath, emberObject, redefineFunctionName);
+  emberObject[redefineFunctionName]();
 }
--- tests/unit/utils/install-get-by-property-test.js
+++ tests/unit/utils/install-get-by-property-test.js
@@ -3,27 +3,30 @@ import { expect } from 'chai';
 import { describe, it, afterEach } from 'mocha';
 import installGetByProperty from 'onedata-gui-common/utils/install-get-by-property';
 
-describe('Unit | Utility | includes-all', function () {
+describe('Unit | Utility | install-get-by-property', function () {
   afterEach(function () {
     this.obj?.destroy();
   });
 
-  it('returns true if superset includes all of subset elements', function () {
+  it('installs property reacting to target changes for the watched property in nested object', function () {
     this.obj = EmberObject.extend({
       watchedPropertyKey: 'foo',
-      internalObj: EmberObject.create({
+      nestedObject: EmberObject.create({
         foo: 1,
         bar: 2,
       }),
       init() {
-        installGetByProperty(this, 'b', 'internalObj', 'watchedPropertyKey');
+        installGetByProperty(this, 'b', 'nestedObject', 'watchedPropertyKey');
       },
     }).create();
 
+    // reads on init
     expect(this.obj.b).to.equal(1);
     this.obj.set('watchedPropertyKey', 'bar');
+    // reads gets value using the new property
     expect(this.obj.b).to.equal(2);
-    this.obj.internalObj.set('bar', '3');
+    // reads value after change
+    this.obj.nestedObject.set('bar', 3);
     expect(this.obj.b).to.equal(3);
   });
 });
