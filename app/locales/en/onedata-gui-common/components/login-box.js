53d11b9d205bf27732b87b85710aedb5416c650f
Jakub Liput <jakub.liput@gmail.com>
2024-09-10 13:00:32 +0200

    VFS-12080 I18n refactor

--- app/locales/en/onedata-gui-common/components/login-box.js
+++ app/locales/en/onedata-gui-common/components/login-box.js
@@ -6,5 +6,5 @@ export default {
   loginFormContainer,
   signIn: 'Sign in',
   errorBackToSignIn: 'Back to sign-in',
-  errorTitle: 'Authentication error!',
+  errorTitle: 'Authentication error',
 };
--- tests/unit/utils/login-view-model-test.js
+++ tests/unit/utils/login-view-model-test.js
@@ -2,10 +2,12 @@ import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import loginViewModel from 'onedata-gui-common/utils/login-view-model';
 
-describe('Unit | Utility | login-view-model', function() {
+// FIXME: implement or delete
+
+describe('Unit | Utility | login-view-model', function () {
   // Replace this with your real tests.
-  it('works', function() {
-    let result = loginViewModel();
+  it('works', function () {
+    const result = loginViewModel();
     expect(result).to.be.ok;
   });
 });
