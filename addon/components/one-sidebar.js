94d66e49de6841b748a622bc1df57ae954f7ad28
Jakub Liput <jakub.liput@gmail.com>
2025-02-04 16:15:38 +0100

    VFS-12554 Removed fetch prev spinner from sidebar (because it shows always after reloading - it must be adjusted to not do so)

--- addon/components/one-sidebar.js
+++ addon/components/one-sidebar.js
@@ -68,12 +68,6 @@ export default Component.extend(I18n, {
   isFilteringEnabled: true,
 
   /**
-   * If true, the spinner will be rendered at the top of the list.
-   * @type {boolean}
-   */
-  isPrevSpinnerShown: false,
-
-  /**
    * If true, the spinner will be rendered at the bottom of the list.
    * @type {boolean}
    */
