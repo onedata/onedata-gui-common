6ff5d545856cfcfe40da2805d5e24aaa65814cf1
Jakub Liput <jakub.liput@gmail.com>
2025-04-28 10:12:59 +0200

    VFS-12673 Added jsdocs; small optimization

--- addon/utils/get-indexed-list-position.js
+++ addon/utils/get-indexed-list-position.js
@@ -1,7 +1,18 @@
-// FIXME: jsdoc
+/**
+ * Find position of item in array as backend does using infinite scroll indexing.
+ *
+ * @author Jakub Liput
+ * @copyright (C) 2025 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ */
 
 import compareStringBytes from './compare-string-bytes';
 
+/**
+ * @param {Array<{ index: string }>} items
+ * @param {string} recordIndex Index of item to search (`index` property).
+ * @returns {number}
+ */
 export default function getIndexedListPosition(items, recordIndex) {
   if (recordIndex === null) {
     return 0;
