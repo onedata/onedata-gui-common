4783f2d88a1e3b08a5d72ac1f18b17b109b6b013
Jakub Liput <jakub.liput@gmail.com>
2025-04-29 13:42:55 +0200

    VFS-12695 Added and refactored TruncatedString tests

--- tests/integration/components/truncated-string-test.js
+++ tests/integration/components/truncated-string-test.js
@@ -4,12 +4,17 @@ import { setupRenderingTest } from 'ember-mocha';
 import { find, render, triggerEvent } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import globals from 'onedata-gui-common/utils/globals';
+import sinon from 'sinon';
+import { htmlSafe } from '@ember/template';
+
+const longText = 'Very very very very very very very very very very very very long text';
 
 describe('Integration | Component | truncated-string', function () {
   setupRenderingTest();
 
   it('renders with passed classname and standard classnames', async function () {
-    await render(hbs`<TruncatedString class="hello">short text</TruncatedString>`);
+    await renderComponent(this, { textContent: 'short text', classname: 'hello' });
+
     const element = find('.hello');
     expect(element, 'truncated string element').to.exist;
     for (const classname of ['truncated-string', 'truncate']) {
@@ -18,117 +23,131 @@ describe('Integration | Component | truncated-string', function () {
   });
 
   it('does not show tooltip, when text is fully visible', async function () {
-    await render(hbs `
-      <div style="min-width: 500px">
-        <TruncatedString>short text</TruncatedString>
-      </div>
-    `);
+    await renderComponent(this, { textContent: longText, isContainerTruncated: false });
+    await hoverTruncatedString();
 
-    await triggerEvent('.truncated-string', 'mouseenter');
     expect(globals.document.querySelector('.tooltip.in')).to.not.exist;
   });
 
   it('shows tooltip with full text, when text is not fully visible', async function () {
-    const longText =
-      'Very very very very very very very very very very very very long text';
-    this.set('longText', longText);
+    await renderComponent(this, { textContent: longText });
+    await hoverTruncatedString();
 
-    await render(hbs `
-      <div style="min-width: 50px; max-width: 50px;">
-        <TruncatedString>{{longText}}</TruncatedString>
-      </div>
-    `);
-
-    await triggerEvent('.truncated-string', 'mouseenter');
     const tooltip = find('.tooltip.in');
     expect(tooltip, '.tooltip.in element').to.exist;
     expect(tooltip.textContent.trim()).to.equal(longText);
   });
 
   it('does not show tooltip, when text isTooltipDisabled is true', async function () {
-    const longText =
-      'Very very very very very very very very very very very very long text';
-    this.set('longText', longText);
+    await renderComponent(this, { textContent: longText, isTooltipDisabled: true });
+    await hoverTruncatedString();
 
-    await render(hbs `
-      <div style="min-width: 50px; max-width: 50px;">
-        <TruncatedString @isTooltipDisabled={{true}}>{{longText}}</TruncatedString>
-      </div>
-    `);
-
-    await triggerEvent('.truncated-string', 'mouseenter');
     const tooltip = find('.tooltip.in');
     expect(tooltip, '.tooltip.in element').to.not.exist;
   });
 
   it('shows custom tooltip text if passed', async function () {
-    const longText =
-      'Very very very very very very very very very very very very long text';
     const customTooltipText = 'hello world custom tooltip';
-    this.setProperties({
-      longText,
-      customTooltipText,
-    });
 
-    await render(hbs `
-      <div style="min-width: 50px; max-width: 50px;">
-        <TruncatedString @customTooltipText={{this.customTooltipText}}>
-          {{longText}}
-        </TruncatedString>
-      </div>
-    `);
+    await renderComponent(this, { textContent: longText, customTooltipText });
+    await hoverTruncatedString();
 
-    await triggerEvent('.truncated-string', 'mouseenter');
     const tooltip = find('.tooltip.in');
-    expect(tooltip.textContent.trim()).to.equal(this.customTooltipText);
+    expect(tooltip.textContent.trim()).to.equal(customTooltipText);
   });
 
   it('removes tooltip element from DOM after mouse leave', async function () {
-    const longText =
-      'Very very very very very very very very very very very very long text';
-    this.set('longText', longText);
-
-    await render(hbs `
-      <div style="min-width: 50px; max-width: 50px;">
-        <TruncatedString>{{longText}}</TruncatedString>
-      </div>
-    `);
+    await renderComponent(this, { textContent: longText });
 
     const findTooltip =
       () => find('.tooltip');
-    await triggerEvent('.truncated-string', 'mouseenter');
+    await hoverTruncatedString();
     expect(findTooltip()).to.exist;
     await triggerEvent('.truncated-string', 'mouseleave');
     expect(findTooltip()).to.not.exist;
   });
 
   it('uses passed tooltipPlacement in tooltip rendering', async function () {
-    const longText =
-      'Very very very very very very very very very very very very long text';
-    this.set('longText', longText);
+    await renderComponent(this, { textContent: longText, tooltipPlacement: 'bottom' });
+    await hoverTruncatedString();
 
-    await render(hbs `
-      <div style="min-width: 50px; max-width: 50px;">
-        <TruncatedString @tooltipPlacement="bottom">{{longText}}</TruncatedString>
-      </div>
-    `);
-
-    await triggerEvent('.truncated-string', 'mouseenter');
     expect(find('.tooltip')).to.have.class('bottom');
   });
 
   it('has top tooltipPlacement by default', async function () {
-    const longText =
-      'Very very very very very very very very very very very very long text';
-    this.set('longText', longText);
+    await renderComponent(this, { textContent: longText });
+    await hoverTruncatedString();
 
-    await render(hbs `
-      <div style="min-width: 50px; max-width: 50px;">
-        <TruncatedString>{{longText}}</TruncatedString>
-      </div>
-    `);
-
-    await triggerEvent('.truncated-string', 'mouseenter');
     expect(find('.tooltip')).to.have.class('top');
   });
+
+  it('uses custom tooltip class if provided', async function () {
+    const tooltipClass = 'hello-world';
+
+    await renderComponent(this, { textContent: longText, tooltipClass });
+    await hoverTruncatedString();
+
+    expect(find('.tooltip')).to.have.class(tooltipClass);
+  });
+
+  it('calls passed tooltipOnShown callback after showing tooltip', async function () {
+    const tooltipOnShown = sinon.spy();
+    await renderComponent(this, { textContent: longText, tooltipOnShown });
+
+    expect(tooltipOnShown).to.have.been.not.called;
+    await hoverTruncatedString();
+    expect(tooltipOnShown).to.have.been.calledOnce;
+  });
+
+  it('does not call passed tooltipOnShown callback after hovering tooltip when tooltip is disabled',
+    async function () {
+      const tooltipOnShown = sinon.spy();
+      await renderComponent(this, { textContent: longText, isTooltipDisabled: true });
+
+      await hoverTruncatedString();
+      expect(tooltipOnShown).to.have.been.not.called;
+    });
 });
+
+async function renderComponent(mochaContext, {
+  textContent,
+  classname,
+  isContainerTruncated = true,
+  isTooltipDisabled,
+  customTooltipText,
+  tooltipPlacement,
+  tooltipOnShown,
+  tooltipClass,
+}) {
+  const containerStyle = htmlSafe(
+    isContainerTruncated ? 'min-width: 50px; max-width: 50px;' : 'min-width: 500px'
+  );
+
+  mochaContext.setProperties({
+    textContent,
+    classname,
+    isTooltipDisabled,
+    customTooltipText,
+    tooltipPlacement,
+    tooltipOnShown,
+    tooltipClass,
+    containerStyle,
+  });
+
+  await render(hbs `
+    <div style={{containerStyle}}>
+      <TruncatedString
+        @isTooltipDisabled={{this.isTooltipDisabled}}
+        @customTooltipText={{this.customTooltipText}}
+        @tooltipPlacement={{this.tooltipPlacement}}
+        @tooltipOnShown={{this.tooltipOnShown}}
+        @tooltipClass={{this.tooltipClass}}
+        class={{this.classname}}
+      >{{textContent}}</TruncatedString>
+    </div>
+  `);
+}
+
+async function hoverTruncatedString() {
+  await triggerEvent('.truncated-string', 'mouseenter');
+}
