a7e016ad2583d6080b942113d92bb29fca280f0e
Jakub Liput <jakub.liput@gmail.com>
2025-04-29 09:47:05 +0200

    VFS-12695 Ported TruncatedString component to Glimmer

--- tests/integration/components/truncated-string-test.js
+++ tests/integration/components/truncated-string-test.js
@@ -1,12 +1,21 @@
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { setupRenderingTest } from 'ember-mocha';
-import { render, triggerEvent } from '@ember/test-helpers';
+import { find, render, triggerEvent } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import globals from 'onedata-gui-common/utils/globals';
 
 describe('Integration | Component | truncated-string', function () {
   setupRenderingTest();
+
+  it('renders with passed classname and standard classnames', async function () {
+    await render(hbs`<TruncatedString class="hello">short text</TruncatedString>`);
+    const element = find('.hello');
+    expect(element, 'truncated string element').to.exist;
+    for (const classname of ['truncated-string', 'truncate']) {
+      expect(element).to.have.class(classname);
+    }
+  });
 
   it('does not show tooltip, when text is fully visible', async function () {
     await render(hbs `
@@ -15,8 +24,8 @@ describe('Integration | Component | truncated-string', function () {
       </div>
     `);
 
-    return triggerEvent('.truncated-string', 'mouseenter')
-      .then(() => expect(globals.document.querySelector('.tooltip.in')).to.not.exist);
+    await triggerEvent('.truncated-string', 'mouseenter');
+    expect(globals.document.querySelector('.tooltip.in')).to.not.exist;
   });
 
   it('shows tooltip with full text, when text is not fully visible', async function () {
@@ -30,10 +39,96 @@ describe('Integration | Component | truncated-string', function () {
       </div>
     `);
 
-    return triggerEvent('.truncated-string', 'mouseenter')
-      .then(() =>
-        expect(globals.document.querySelector('.tooltip.in').textContent.trim())
-        .to.equal(longText)
-      );
+    await triggerEvent('.truncated-string', 'mouseenter');
+    const tooltip = find('.tooltip.in');
+    expect(tooltip, '.tooltip.in element').to.exist;
+    expect(tooltip.textContent.trim()).to.equal(longText);
+  });
+
+  it('does not show tooltip, when text isTooltipDisabled is true', async function () {
+    const longText =
+      'Very very very very very very very very very very very very long text';
+    this.set('longText', longText);
+
+    await render(hbs `
+      <div style="min-width: 50px; max-width: 50px;">
+        <TruncatedString @isTooltipDisabled={{true}}>{{longText}}</TruncatedString>
+      </div>
+    `);
+
+    await triggerEvent('.truncated-string', 'mouseenter');
+    const tooltip = find('.tooltip.in');
+    expect(tooltip, '.tooltip.in element').to.not.exist;
+  });
+
+  it('shows custom tooltip text if passed', async function () {
+    const longText =
+      'Very very very very very very very very very very very very long text';
+    const customTooltipText = 'hello world custom tooltip';
+    this.setProperties({
+      longText,
+      customTooltipText,
+    });
+
+    await render(hbs `
+      <div style="min-width: 50px; max-width: 50px;">
+        <TruncatedString @customTooltipText={{this.customTooltipText}}>
+          {{longText}}
+        </TruncatedString>
+      </div>
+    `);
+
+    await triggerEvent('.truncated-string', 'mouseenter');
+    const tooltip = find('.tooltip.in');
+    expect(tooltip.textContent.trim()).to.equal(this.customTooltipText);
+  });
+
+  it('removes tooltip element from DOM after mouse leave', async function () {
+    const longText =
+      'Very very very very very very very very very very very very long text';
+    this.set('longText', longText);
+
+    await render(hbs `
+      <div style="min-width: 50px; max-width: 50px;">
+        <TruncatedString>{{longText}}</TruncatedString>
+      </div>
+    `);
+
+    const findTooltip =
+      () => find('.tooltip');
+    await triggerEvent('.truncated-string', 'mouseenter');
+    expect(findTooltip()).to.exist;
+    await triggerEvent('.truncated-string', 'mouseleave');
+    expect(findTooltip()).to.not.exist;
+  });
+
+  it('uses passed tooltipPlacement in tooltip rendering', async function () {
+    const longText =
+      'Very very very very very very very very very very very very long text';
+    this.set('longText', longText);
+
+    await render(hbs `
+      <div style="min-width: 50px; max-width: 50px;">
+        <TruncatedString @tooltipPlacement="bottom">{{longText}}</TruncatedString>
+      </div>
+    `);
+
+    await triggerEvent('.truncated-string', 'mouseenter');
+    expect(find('.tooltip')).to.have.class('bottom');
+  });
+
+  it('has top tooltipPlacement by default', async function () {
+    const longText =
+      'Very very very very very very very very very very very very long text';
+    this.set('longText', longText);
+
+    await render(hbs `
+      <div style="min-width: 50px; max-width: 50px;">
+        <TruncatedString>{{longText}}</TruncatedString>
+      </div>
+    `);
+
+    await triggerEvent('.truncated-string', 'mouseenter');
+    expect(find('.tooltip')).to.have.class('top');
   });
 });
