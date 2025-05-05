33bfbba83df06806e5c44baea42a49543dd93496
Jakub Liput <jakub.liput@gmail.com>
2025-05-05 14:39:22 +0200

    VFS-12758 Fixed cutting down sidebar chunkable list after filtering changes

--- addon/utils/chunkable-list-model.js
+++ addon/utils/chunkable-list-model.js
@@ -20,6 +20,12 @@ import ChunkableListModelReloader from './chunkable-list-model-reloader';
 import _ from 'lodash';
 
 export class ChunkableListModel {
+  /**
+   * Maximum initial size of created chunks array.
+   * @type {number}
+   */
+  initialArraySize = 50;
+
   /** @type {typeof ChunkableListModelFetcher} */
   get ChunkableListModelFetcherClass() {
     return ChunkableListModelFetcher;
@@ -67,7 +73,7 @@ export class ChunkableListModel {
         return this.chunkableListModelFetcher.fetch(index, limit, offset);
       },
       startIndex: 0,
-      endIndex: 50,
+      endIndex: this.initialArraySize,
       indexMargin: 10,
       // TODO: VFS-12726 Remove chunkSize hack that fixes sidebar jump
       chunkSize: 10,
@@ -79,6 +85,7 @@ export class ChunkableListModel {
     this.chunkableListModelReloader = ChunkableListModelReloader.create({
       listModel,
       chunksArray: this.chunksArray,
+      initialArraySize: this.initialArraySize,
     });
   }
 
@@ -95,7 +102,7 @@ export class ChunkableListModel {
       return;
     }
     this.chunkableListModelFetcher.setFilter({ expression, advanced });
-    this.chunkableListModelReloader.handleListChange();
+    this.chunkableListModelReloader.handleListChange({ reset: true });
   }
 }
 
