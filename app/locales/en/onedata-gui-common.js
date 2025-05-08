1359753d9694a97ef46716a45af167d407269108
Jakub Liput <jakub.liput@gmail.com>
2025-05-08 21:27:51 +0200

    VFS-12765 Removed providers-map-tile from common

--- app/locales/en/onedata-gui-common.js
+++ app/locales/en/onedata-gui-common.js
@@ -130,7 +130,6 @@ const translations = {
     authenticationErrorModal,
     sidebarClusters,
     clipboardLine,
-    providersMapTile,
     userAccountButtonBase,
     brandInfo,
     jsonEditor,
