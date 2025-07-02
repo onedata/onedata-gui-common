f0ffe3e1eec2e6959b9197222c1f8ea5888c511a
Jakub Liput <jakub.liput@gmail.com>
2025-07-02 14:41:16 +0200

    VFS-12759 Fixed and added tests to ChunkablePlainArray

--- tests/unit/utils/chunkable-plain-array-test.js
+++ tests/unit/utils/chunkable-plain-array-test.js
@@ -9,8 +9,7 @@ describe('Unit | Utility | chunkable-plain-array', function () {
     this.chunkable?.destroy();
   });
 
-  it(
-    'exposes chosen fragment via ReplacingChunksArray with data source from sourceArray',
+  it('exposes chosen fragment via ReplacingChunksArray with data source from sourceArray',
     async function () {
       const sourceArray = _.range(100).map(i => createItem(i));
       this.chunkable = new ChunkablePlainArray(sourceArray);
@@ -21,7 +20,9 @@ describe('Unit | Utility | chunkable-plain-array', function () {
       await settled();
 
       const actual = chunksArray.toArray();
-      const expected = _.range(10, 40).map(i => new IndexedItem(createItem(i), i));
+      const expected = _.range(10, 40).map(i =>
+        new IndexedItem(createItem(i), String(i))
+      );
       expect(actual).to.deep.equal(expected);
     }
   );
@@ -29,7 +30,7 @@ describe('Unit | Utility | chunkable-plain-array', function () {
   it('allows to jump to array index via ReplacingChunksArray', async function () {
     const sourceArray = _.range(100).map(i => createItem(i));
     this.chunkable = new ChunkablePlainArray(sourceArray, {
-      initialJumpIndex: 80,
+      initialJumpIndex: '80',
       minChunkSize: 20,
       indexMargin: 0,
     });
@@ -38,9 +39,34 @@ describe('Unit | Utility | chunkable-plain-array', function () {
     await chunksArray.initialLoad;
 
     const actual = chunksArray.toArray();
-    const expected = _.range(80, 100).map(i => new IndexedItem(createItem(i), i));
+    const expected = _.range(80, 100).map(i => new IndexedItem(createItem(i), String(i)));
     expect(actual).to.deep.equal(expected);
   });
+
+  it('exposes new source plain array after source change',
+    async function () {
+      const sourceArray1 = _.range(100).map(i => createItem(i));
+      const sourceArray2 = _.range(100, 200).map(i => createItem(i));
+      this.chunkable = new ChunkablePlainArray(sourceArray1, { indexMargin: 0 });
+
+      const chunksArray = this.chunkable.chunksArray;
+      await chunksArray.initialLoad;
+      chunksArray.setIndices(0, 10);
+      await settled();
+      const expected1 = _.range(0, 10).map(i =>
+        new IndexedItem(createItem(i), String(i))
+      );
+      expect(chunksArray.toArray()).to.deep.equal(expected1);
+
+      // change source array
+      this.chunkable.sourceArray = sourceArray2;
+      await settled();
+      const expected2 = _.range(0, 10).map(i =>
+        new IndexedItem(createItem(i + 100), String(i))
+      );
+      expect(chunksArray.toArray()).to.deep.equal(expected2);
+    }
+  );
 });
 
 function createItem(i) {
