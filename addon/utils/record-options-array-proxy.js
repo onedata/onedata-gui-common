7ec2ccbc33ea4e1955229930f5c2d7082266b9a1
Jakub Liput <jakub.liput@gmail.com>
2025-10-16 15:54:11 +0200

    VFS-12896 Fixed conflict label filtering in RecordOptionsArrayProxy-based dropdowns

--- addon/utils/record-options-array-proxy.js
+++ addon/utils/record-options-array-proxy.js
@@ -2,8 +2,10 @@
  * A proxy array that transforms source array to an array of options ready to use
  * by dropdown or other enumeration mechanism. Extracts label and correct icon.
  *
- * @author Michał Borzęcki
+ * @author Michał Borzęcki, Jakub Liput
  * @copyright (C) 2020 ACK CYFRONET AGH
+ * @copyright (C) 2025 Onedata (onedata.org)
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
 
 import ArrayProxy from '@ember/array/proxy';
@@ -28,11 +30,16 @@ export default ArrayProxy.extend({
    * @override
    */
   content: computed('sortedRecords.@each.{name,conflictLabel}', function content() {
-    return this.get('sortedRecords').map(record => ({
-      value: record,
-      label: get(record, 'name'),
-      searchableName: `${record.name}${record.conflictLabel && defaultSeparator}${record.conflictLabel}`,
-      icon: recordIcon(record),
-    }));
+    return this.sortedRecords.map(record => {
+      const searchableName = record.conflictLabel ?
+        `${record.name}${defaultSeparator}${record.conflictLabel}` :
+        record.name;
+      return {
+        value: record,
+        label: get(record, 'name'),
+        searchableName,
+        icon: recordIcon(record),
+      };
+    });
   }),
 });
