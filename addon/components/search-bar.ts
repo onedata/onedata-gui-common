64941bb96430877c1723304f9a71d7f498344715
plgagneska <agawarchol15@gmail.com>
2026-05-14 19:27:22 +0200

    VFS-13395 Fixed search text in sidebar

--- addon/components/search-bar.ts
+++ addon/components/search-bar.ts
@@ -13,12 +13,17 @@ import Locale from 'onedata-gui-common/utils/locale';
 interface SearchBarSignature {
   Args: {
     search: (expression: string) => void;
+    customPlaceholder: string | undefined;
   };
   Element: HTMLInputElement;
 }
 
 export default class SearchBar extends Component<SearchBarSignature> {
   locale = new Locale('components.searchBar');
+
+  get placeholder() {
+    return this.args.customPlaceholder || this.locale.t('searchPlaceholder');
+  }
 
   @action
   handleInput(event: InputEvent) {
